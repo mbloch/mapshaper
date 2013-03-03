f5203587a9216956f0e38eaa554e57532cf47b1e
Matthew Bloch <masiyou@gmail.com>
2013-03-03 12:11:45 -0500

    Rebuild lib file, etc.

--- src/mapshaper-repair.js
+++ src/mapshaper-repair.js
@@ -6,27 +6,6 @@
 //
 
 MapShaper.repairTopology = function(obj, chainedIds, bb, resolution) {
-  var minx = bb.left,
-      maxx = bb.right,
-      miny = bb.bottom,
-      maxy = bb.top;
-
-  var MAX_SIDE = 40000;
-
-  var rows = Math.ceil((maxy - miny) / resolution);
-      cols = Math.ceil((maxx - minx) / resolution);
-
-  function getRow(y) {
-
-  }
-
-  function getCol(x) {
-
-  }
-
-  function hash(x, y) {
-    
-  }
 
 
 };
