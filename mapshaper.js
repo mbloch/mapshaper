fa270425e13c4a26c5a86f7f0e7a0bba2bae8fe3
Matthew Bloch <masiyou@gmail.com>
2013-06-24 19:58:59 -0700

    Recat

--- mapshaper.js
+++ mapshaper.js
@@ -412,6 +412,295 @@ var T = {
 };
 
 
+
+function Transform() {
+  this.mx = this.my = 1;
+  this.bx = this.by = 0;
+}
+
+Transform.prototype.invert = function() {
+  var inv = new Transform();
+  inv.mx = 1 / this.mx;
+  inv.my = 1 / this.my;
+  inv.bx = -this.bx / this.mx;
+  inv.by = -this.by / this.my;
+  return inv;
+};
+
+
+/*
+Transform.prototype.useTileBounds = function(wPix, hPix, bb) {
+  var ppm = wPix / (bb.right - bb.left);
+  this.mx = ppm;
+  this.my = hPix / (bb.bottom - bb.top);
+  this.bx = -ppm * bb.left;
+  this.by = -this.my * bb.top;
+  return this;
+};
+*/
+
+Transform.prototype.transform = function(x, y, xy) {
+  xy = xy || [];
+  xy[0] = x * this.mx + this.bx;
+  xy[1] = y * this.my + this.by;
+  return xy;
+};
+
+// Transform.prototype.toString = function() {};
+
+function Bounds() {
+  if (arguments.length > 0) {
+    this.setBounds.apply(this, arguments);
+  }
+}
+
+Bounds.prototype.toString = function() {
+  return JSON.stringify({
+    xmin: this.xmin,
+    xmax: this.xmax,
+    ymin: this.ymin,
+    ymax: this.ymax
+  });
+};
+
+Bounds.prototype.toArray = function() {
+  return [this.xmin, this.ymin, this.xmax, this.ymax];
+};
+
+Bounds.prototype.hasBounds = function() {
+  return !isNaN(this.ymax);
+};
+
+Bounds.prototype.sameBounds =
+Bounds.prototype.equals = function(bb) {
+  return bb && this.xmin === bb.xmin && this.xmax === bb.xmax &&
+    this.ymin === bb.ymin && this.ymax === bb.ymax;
+};
+
+Bounds.prototype.width = function() {
+  return (this.xmax - this.xmin) || 0;
+};
+
+Bounds.prototype.height = function() {
+  return (this.ymax - this.ymin) || 0;
+};
+
+Bounds.prototype.setBounds = function(a, b, c, d) {
+  if (arguments.length == 1) {
+    // assume first arg is a Bounds or array
+    if (Utils.isArrayLike(a)) {
+      b = a[1];
+      c = a[2];
+      d = a[3];
+      a = a[0];
+    } else {
+      b = a.ymin;
+      c = a.xmax;
+      d = a.ymax;
+      a = a.xmin;
+    }
+  }
+  if (a > c || b > d) error("Bounds#setBounds() min/max reversed:", a, b, c, d);
+  this.xmin = a;
+  this.ymin = b;
+  this.xmax = c;
+  this.ymax = d;
+  return this;
+};
+
+/*
+Bounds.prototype.getCenterPoint = function() {
+  if (!this.hasBounds()) error("Missing bounds");
+  return new Point(this.centerX(), this.centerY());
+};
+*/
+
+Bounds.prototype.centerX = function() {
+  var x = (this.xmin + this.xmax) * 0.5;
+  return x;
+};
+
+Bounds.prototype.centerY = function() {
+  var y = (this.ymax + this.ymin) * 0.5;
+  return y;
+};
+
+Bounds.prototype.containsPoint = function(x, y) {
+  if (x >= this.xmin && x <= this.xmax &&
+    y <= this.ymax && y >= this.ymin) {
+    return true;
+  }
+  return false;
+};
+
+// intended to speed up slightly bubble symbol detection; could use intersects() instead
+// * FIXED * may give false positives if bubbles are located outside corners of the box
+//
+Bounds.prototype.containsBufferedPoint = function( x, y, buf ) {
+  if ( x + buf > this.xmin && x - buf < this.xmax ) {
+    if ( y - buf < this.ymax && y + buf > this.ymin ) {
+      return true;
+    }
+  }
+  return false;
+};
+
+Bounds.prototype.intersects = function(bb) {
+  if (bb.xmin <= this.xmax && bb.xmax >= this.xmin &&
+    bb.ymax >= this.ymin && bb.ymin <= this.ymax) {
+    return true;
+  }
+  return false;
+};
+
+Bounds.prototype.contains = function(bb) {
+  if (bb.xmin >= this.xmin && bb.ymax <= this.ymax &&
+    bb.xmax <= this.xmax && bb.ymin >= this.ymin) {
+    return true;
+  }
+  return false;
+};
+
+Bounds.prototype.shift = function(x, y) {
+  this.setBounds(this.xmin + x,
+    this.ymin + y, this.xmax + x, this.ymax + y);
+};
+
+Bounds.prototype.padBounds = function(a, b, c, d) {
+  this.xmin -= a;
+  this.ymin -= b;
+  this.xmax += c;
+  this.ymax += d;
+};
+
+/**
+ * Rescale the bounding box by a fraction. TODO: implement focus.
+ * @param {number} pct Fraction of original extents
+ * @param {number} pctY Optional amount to scale Y
+ */
+Bounds.prototype.scale = function(pct, pctY) { /*, focusX, focusY*/
+  var halfWidth = (this.xmax - this.xmin) * 0.5;
+  var halfHeight = (this.ymax - this.ymin) * 0.5;
+  var kx = pct - 1;
+  var ky = pctY === undefined ? kx : pctY - 1;
+  this.xmin -= halfWidth * kx;
+  this.ymin -= halfHeight * ky;
+  this.xmax += halfWidth * kx;
+  this.ymax += halfHeight * ky;
+};
+
+/**
+ * Return a bounding box with the same extent as this one.
+ */
+Bounds.prototype.cloneBounds = // alias so child classes can override clone()
+Bounds.prototype.clone = function() {
+  return new Bounds(this.xmin, this.ymin, this.xmax, this.ymax);
+};
+
+Bounds.prototype.clearBounds = function() {
+  this.setBounds(new Bounds());
+};
+
+Bounds.prototype.mergePoint = function(x, y) {
+  if (this.xmin === void 0) {
+    this.setBounds(x, y, x, y);
+  } else {
+    // this works even if x,y are NaN
+    if (x < this.xmin)  this.xmin = x;
+    else if (x > this.xmax)  this.xmax = x;
+
+    if (y < this.ymin) this.ymin = y;
+    else if (y > this.ymax) this.ymax = y;
+  }
+};
+
+// TODO: pick a better name
+// expands either x or y dimension to match @aspect (width/height ratio)
+// @focusX, @focusY (optional): expansion focus, as a fraction of width and height
+//
+Bounds.prototype.fillOut = function(aspect, focusX, focusY) {
+  if (arguments.length < 3) {
+    focusX = 0.5;
+    focusY = 0.5;
+  }
+  var w = this.width(),
+      h = this.height(),
+      currAspect = w / h,
+      pad;
+  if (currAspect < aspect) { // fill out x dimension
+    pad = h * aspect - w;
+    this.xmin -= (1 - focusX) * pad;
+    this.xmax += focusX * pad;
+  } else {
+    pad = w / aspect - h;
+    this.ymin -= (1 - focusY) * pad;
+    this.ymax += focusY * pad;
+  }
+  return this;
+};
+
+Bounds.prototype.update = function() {
+  var tmp;
+  if (this.xmin > this.xmax) {
+    tmp = this.xmin;
+    this.xmin = this.xmax;
+    this.xmax = tmp;
+  }
+  if (this.ymin > this.ymax) {
+    tmp = this.ymin;
+    this.ymin = this.ymax;
+    this.ymax = tmp;
+  }
+};
+
+Bounds.prototype.transform = function(t) {
+  this.xmin = this.xmin * t.mx + t.bx;
+  this.xmax = this.xmax * t.mx + t.bx;
+  this.ymin = this.ymin * t.my + t.by;
+  this.ymax = this.ymax * t.my + t.by;
+  this.update();
+  return this;
+};
+
+// Returns a Transform object for mapping this onto Bounds @b2
+// @flipY (optional) Flip y-axis coords, for converting to/from pixel coords
+//
+Bounds.prototype.getTransform = function(b2, flipY) {
+  var t = new Transform();
+  t.mx = b2.width() / this.width();
+  t.bx = b2.xmin - t.mx * this.xmin;
+  if (flipY) {
+    t.my = -b2.height() / this.height();
+    t.by = b2.ymax - t.my * this.ymin;
+  } else {
+    t.my = b2.height() / this.height();
+    t.by = b2.ymin - t.my * this.ymin;
+  }
+  return t;
+};
+
+Bounds.prototype.mergeBounds = function(bb) {
+  var a, b, c, d;
+  if (bb.xmin !== void 0) {
+    a = bb.xmin, b = bb.ymin, c = bb.xmax, d = bb.ymax;
+  } else if (bb.length == 4) {
+    a = bb[0], b = bb[1], c = bb[2], d = bb[3]; // expects array: [xmin, ymin, xmax, ymax]
+  } else {
+    error("Bounds#mergeBounds() invalid argument:", bb);
+  }
+
+  if (this.xmin === void 0) {
+    this.setBounds(a, b, c, d);
+  } else {
+    if (a < this.xmin) this.xmin = a;
+    if (b < this.ymin) this.ymin = b;
+    if (c > this.xmax) this.xmax = c;
+    if (d > this.ymax) this.ymax = d;
+  }
+  return this;
+};
+
+
 /** @requires core */
 
 function Handler(type, target, callback, listener, priority) {
@@ -5285,295 +5574,6 @@ DouglasPeucker.calcArcData = function(xx, yy, zz, len) {
   }
 
   return dpArr;
-};
-
-
-
-function Transform() {
-  this.mx = this.my = 1;
-  this.bx = this.by = 0;
-}
-
-Transform.prototype.invert = function() {
-  var inv = new Transform();
-  inv.mx = 1 / this.mx;
-  inv.my = 1 / this.my;
-  inv.bx = -this.bx / this.mx;
-  inv.by = -this.by / this.my;
-  return inv;
-};
-
-
-/*
-Transform.prototype.useTileBounds = function(wPix, hPix, bb) {
-  var ppm = wPix / (bb.right - bb.left);
-  this.mx = ppm;
-  this.my = hPix / (bb.bottom - bb.top);
-  this.bx = -ppm * bb.left;
-  this.by = -this.my * bb.top;
-  return this;
-};
-*/
-
-Transform.prototype.transform = function(x, y, xy) {
-  xy = xy || [];
-  xy[0] = x * this.mx + this.bx;
-  xy[1] = y * this.my + this.by;
-  return xy;
-};
-
-// Transform.prototype.toString = function() {};
-
-function Bounds() {
-  if (arguments.length > 0) {
-    this.setBounds.apply(this, arguments);
-  }
-}
-
-Bounds.prototype.toString = function() {
-  return JSON.stringify({
-    xmin: this.xmin,
-    xmax: this.xmax,
-    ymin: this.ymin,
-    ymax: this.ymax
-  });
-};
-
-Bounds.prototype.toArray = function() {
-  return [this.xmin, this.ymin, this.xmax, this.ymax];
-};
-
-Bounds.prototype.hasBounds = function() {
-  return !isNaN(this.ymax);
-};
-
-Bounds.prototype.sameBounds =
-Bounds.prototype.equals = function(bb) {
-  return bb && this.xmin === bb.xmin && this.xmax === bb.xmax &&
-    this.ymin === bb.ymin && this.ymax === bb.ymax;
-};
-
-Bounds.prototype.width = function() {
-  return (this.xmax - this.xmin) || 0;
-};
-
-Bounds.prototype.height = function() {
-  return (this.ymax - this.ymin) || 0;
-};
-
-Bounds.prototype.setBounds = function(a, b, c, d) {
-  if (arguments.length == 1) {
-    // assume first arg is a Bounds or array
-    if (Utils.isArrayLike(a)) {
-      b = a[1];
-      c = a[2];
-      d = a[3];
-      a = a[0];
-    } else {
-      b = a.ymin;
-      c = a.xmax;
-      d = a.ymax;
-      a = a.xmin;
-    }
-  }
-  if (a > c || b > d) error("Bounds#setBounds() min/max reversed:", a, b, c, d);
-  this.xmin = a;
-  this.ymin = b;
-  this.xmax = c;
-  this.ymax = d;
-  return this;
-};
-
-/*
-Bounds.prototype.getCenterPoint = function() {
-  if (!this.hasBounds()) error("Missing bounds");
-  return new Point(this.centerX(), this.centerY());
-};
-*/
-
-Bounds.prototype.centerX = function() {
-  var x = (this.xmin + this.xmax) * 0.5;
-  return x;
-};
-
-Bounds.prototype.centerY = function() {
-  var y = (this.ymax + this.ymin) * 0.5;
-  return y;
-};
-
-Bounds.prototype.containsPoint = function(x, y) {
-  if (x >= this.xmin && x <= this.xmax &&
-    y <= this.ymax && y >= this.ymin) {
-    return true;
-  }
-  return false;
-};
-
-// intended to speed up slightly bubble symbol detection; could use intersects() instead
-// * FIXED * may give false positives if bubbles are located outside corners of the box
-//
-Bounds.prototype.containsBufferedPoint = function( x, y, buf ) {
-  if ( x + buf > this.xmin && x - buf < this.xmax ) {
-    if ( y - buf < this.ymax && y + buf > this.ymin ) {
-      return true;
-    }
-  }
-  return false;
-};
-
-Bounds.prototype.intersects = function(bb) {
-  if (bb.xmin <= this.xmax && bb.xmax >= this.xmin &&
-    bb.ymax >= this.ymin && bb.ymin <= this.ymax) {
-    return true;
-  }
-  return false;
-};
-
-Bounds.prototype.contains = function(bb) {
-  if (bb.xmin >= this.xmin && bb.ymax <= this.ymax &&
-    bb.xmax <= this.xmax && bb.ymin >= this.ymin) {
-    return true;
-  }
-  return false;
-};
-
-Bounds.prototype.shift = function(x, y) {
-  this.setBounds(this.xmin + x,
-    this.ymin + y, this.xmax + x, this.ymax + y);
-};
-
-Bounds.prototype.padBounds = function(a, b, c, d) {
-  this.xmin -= a;
-  this.ymin -= b;
-  this.xmax += c;
-  this.ymax += d;
-};
-
-/**
- * Rescale the bounding box by a fraction. TODO: implement focus.
- * @param {number} pct Fraction of original extents
- * @param {number} pctY Optional amount to scale Y
- */
-Bounds.prototype.scale = function(pct, pctY) { /*, focusX, focusY*/
-  var halfWidth = (this.xmax - this.xmin) * 0.5;
-  var halfHeight = (this.ymax - this.ymin) * 0.5;
-  var kx = pct - 1;
-  var ky = pctY === undefined ? kx : pctY - 1;
-  this.xmin -= halfWidth * kx;
-  this.ymin -= halfHeight * ky;
-  this.xmax += halfWidth * kx;
-  this.ymax += halfHeight * ky;
-};
-
-/**
- * Return a bounding box with the same extent as this one.
- */
-Bounds.prototype.cloneBounds = // alias so child classes can override clone()
-Bounds.prototype.clone = function() {
-  return new Bounds(this.xmin, this.ymin, this.xmax, this.ymax);
-};
-
-Bounds.prototype.clearBounds = function() {
-  this.setBounds(new Bounds());
-};
-
-Bounds.prototype.mergePoint = function(x, y) {
-  if (this.xmin === void 0) {
-    this.setBounds(x, y, x, y);
-  } else {
-    // this works even if x,y are NaN
-    if (x < this.xmin)  this.xmin = x;
-    else if (x > this.xmax)  this.xmax = x;
-
-    if (y < this.ymin) this.ymin = y;
-    else if (y > this.ymax) this.ymax = y;
-  }
-};
-
-// TODO: pick a better name
-// expands either x or y dimension to match @aspect (width/height ratio)
-// @focusX, @focusY (optional): expansion focus, as a fraction of width and height
-//
-Bounds.prototype.fillOut = function(aspect, focusX, focusY) {
-  if (arguments.length < 3) {
-    focusX = 0.5;
-    focusY = 0.5;
-  }
-  var w = this.width(),
-      h = this.height(),
-      currAspect = w / h,
-      pad;
-  if (currAspect < aspect) { // fill out x dimension
-    pad = h * aspect - w;
-    this.xmin -= (1 - focusX) * pad;
-    this.xmax += focusX * pad;
-  } else {
-    pad = w / aspect - h;
-    this.ymin -= (1 - focusY) * pad;
-    this.ymax += focusY * pad;
-  }
-  return this;
-};
-
-Bounds.prototype.update = function() {
-  var tmp;
-  if (this.xmin > this.xmax) {
-    tmp = this.xmin;
-    this.xmin = this.xmax;
-    this.xmax = tmp;
-  }
-  if (this.ymin > this.ymax) {
-    tmp = this.ymin;
-    this.ymin = this.ymax;
-    this.ymax = tmp;
-  }
-};
-
-Bounds.prototype.transform = function(t) {
-  this.xmin = this.xmin * t.mx + t.bx;
-  this.xmax = this.xmax * t.mx + t.bx;
-  this.ymin = this.ymin * t.my + t.by;
-  this.ymax = this.ymax * t.my + t.by;
-  this.update();
-  return this;
-};
-
-// Returns a Transform object for mapping this onto Bounds @b2
-// @flipY (optional) Flip y-axis coords, for converting to/from pixel coords
-//
-Bounds.prototype.getTransform = function(b2, flipY) {
-  var t = new Transform();
-  t.mx = b2.width() / this.width();
-  t.bx = b2.xmin - t.mx * this.xmin;
-  if (flipY) {
-    t.my = -b2.height() / this.height();
-    t.by = b2.ymax - t.my * this.ymin;
-  } else {
-    t.my = b2.height() / this.height();
-    t.by = b2.ymin - t.my * this.ymin;
-  }
-  return t;
-};
-
-Bounds.prototype.mergeBounds = function(bb) {
-  var a, b, c, d;
-  if (bb.xmin !== void 0) {
-    a = bb.xmin, b = bb.ymin, c = bb.xmax, d = bb.ymax;
-  } else if (bb.length == 4) {
-    a = bb[0], b = bb[1], c = bb[2], d = bb[3]; // expects array: [xmin, ymin, xmax, ymax]
-  } else {
-    error("Bounds#mergeBounds() invalid argument:", bb);
-  }
-
-  if (this.xmin === void 0) {
-    this.setBounds(a, b, c, d);
-  } else {
-    if (a < this.xmin) this.xmin = a;
-    if (b < this.ymin) this.ymin = b;
-    if (c > this.xmax) this.xmax = c;
-    if (d > this.ymax) this.ymax = d;
-  }
-  return this;
 };
 
 
