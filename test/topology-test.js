c6a237944197edab278f8322c1ed4cd6b77ce3d9
Matthew Bloch <masiyou@gmail.com>
2013-07-08 23:45:44 -0400

    Make topology tests easier to interpret

--- test/topology-test.js
+++ test/topology-test.js
@@ -8,6 +8,13 @@ describe("mapshaper-topology.js", function() {
 
   describe('irregular holes', function () {
     it('hole shares two segments with enclosing path', function () {
+
+          //      b --- c
+          //     / \   /
+          //    /   \ /
+          //   a --- d
+
+          //   [a, b, c, d, a] [a, d, b, a]
       var yy = [1, 3, 3, 1, 1,  1, 1, 3, 1],
           xx = [1, 2, 4, 3, 1,  1, 3, 2, 1],
           pathData = [{isHole: false, size: 5}, {isHole: true, size: 4}];
@@ -19,7 +26,14 @@ describe("mapshaper-topology.js", function() {
   })
 
   describe('two rings share one point', function () {
+
+    //      b     d
+    //     / \   / \
+    //    /   \ /   \
+    //   a --- c --- e
+
     it('shared point is endpoint of one ring', function () {
+          //   [a, b, c, a] [c, d, e, c]
       var yy = [1, 3, 1, 1,  1, 3, 1, 1],
           xx = [1, 2, 3, 1,  3, 4, 5, 3],
           pathData = [{isHole: false, size: 4}, {isHole: false, size: 4}];
@@ -30,6 +44,7 @@ describe("mapshaper-topology.js", function() {
     })
 
     it('shared point is endpoint of neither ring', function () {
+          //   [a, b, c, a] [e, c, d, e]
       var yy = [1, 3, 1, 1,  3, 1, 1, 3],
           xx = [1, 2, 3, 1,  4, 5, 3, 4],
           pathData = [{isHole: false, size: 4}, {isHole: false, size: 4}];
@@ -42,7 +57,14 @@ describe("mapshaper-topology.js", function() {
   })
 
   describe('congruent rings', function () {
+
+    //      b
+    //     / \
+    //    /   \
+    //   a --- c
+
     it('hole-around-island, aligned', function () {
+          //   [a, b, c, a] [a, c, b, a]
       var yy = [1, 3, 1, 1,  1, 1, 3, 1],
           xx = [1, 2, 3, 1,  1, 3, 2, 1],
           pathData = [{isHole: false, size: 4}, {isHole: true, size: 4}];
@@ -53,6 +75,7 @@ describe("mapshaper-topology.js", function() {
     })
 
     it('hole-around-island, misaligned', function () {
+          //   [a, b, c, a] [c, b, a, c]
       var yy = [1, 3, 1, 1,  1, 3, 1, 1],
           xx = [1, 2, 3, 1,  3, 2, 1, 3],
           pathData = [{isHole: false, size: 4}, {isHole: true, size: 4}];
@@ -63,6 +86,7 @@ describe("mapshaper-topology.js", function() {
     })
 
     it('duplicate islands, aligned', function () {
+          //   [a, b, c, a] [a, b, c, a]
       var yy = [1, 3, 1, 1,  1, 3, 1, 1],
           xx = [1, 2, 3, 1,  1, 2, 3, 1],
           pathData = [{isHole: false, size: 4}, {isHole: false, size: 4}];
@@ -72,6 +96,7 @@ describe("mapshaper-topology.js", function() {
     })
 
     it('duplicate islands, misaligned', function () {
+          //   [a, b, c, a] [c, a, b, c]
       var yy = [1, 3, 1, 1,  1, 1, 3, 1],
           xx = [1, 2, 3, 1,  3, 1, 2, 3],
           pathData = [{isHole: false, size: 4}, {isHole: false, size: 4}];
@@ -81,6 +106,7 @@ describe("mapshaper-topology.js", function() {
     })
 
     it('three duplicate islands, aligned', function () {
+          //   [a, b, c, a] [a, b, c, a] [a, b, c, a]
       var yy = [1, 3, 1, 1,  1, 3, 1, 1,  1, 3, 1, 1],
           xx = [1, 2, 3, 1,  1, 2, 3, 1,  1, 2, 3, 1],
           pathData = [{isHole: false, size: 4}, {isHole: false, size: 4}, {isHole: false, size: 4}];
@@ -90,6 +116,7 @@ describe("mapshaper-topology.js", function() {
     })
 
     it('three duplicate islands, misaligned', function () {
+          //   [a, b, c, a] [c, a, b, c] [b, c, a, b]
       var yy = [1, 3, 1, 1,  1, 1, 3, 1,  3, 1, 1, 3],
           xx = [1, 2, 3, 1,  3, 1, 2, 3,  2, 3, 1, 2],
           pathData = [{isHole: false, size: 4}, {isHole: false, size: 4}, {isHole: false, size: 4}];
@@ -99,6 +126,7 @@ describe("mapshaper-topology.js", function() {
     })
 
     it('two duplicate islands and a hole, misaligned', function () {
+          //   [a, b, c, a] [c, a, b, c] [b, a, c, b]
       var yy = [1, 3, 1, 1,  1, 1, 3, 1,  3, 1, 1, 3],
           xx = [1, 2, 3, 1,  3, 1, 2, 3,  2, 1, 3, 2],
           pathData = [{isHole: false, size: 4}, {isHole: false, size: 4}, {isHole: true, size: 4}];
@@ -109,65 +137,55 @@ describe("mapshaper-topology.js", function() {
 
   })
 
-  describe("buildTopology()", function() {
 
-    var d1 = {
-      yy:       [1, 1, 3, 1,  3, 3, 1, 3],
-      xx:       [3, 1, 2, 3,  2, 4, 3, 2],
-      pathData: [{shapeId: 0, size: 4}, {shapeId: 1, size: 4}]
+  describe('two rings, one shared segment', function () {
 
-    };
+    //      b --- d
+    //     / \   /
+    //    /   \ /
+    //   a --- c
 
-    var d2 = {
-      yy:       [1, 1, 3, 1,  1, 3, 3, 1],
-      xx:       [3, 1, 2, 3,  3, 2, 4, 3],
-      pathData: [{shapeId: 0, size: 4}, {shapeId: 1, size: 4}]
-    };
+    it('ring endpoints are aligned', function () {
+          //   [c, a, b, c] [c, b, d, c]
+      var yy = [1, 1, 3, 1,  1, 3, 3, 1],
+          xx = [3, 1, 2, 3,  3, 2, 4, 3],
+          pathData = [{isHole: false, size: 4}, {isHole: false, size: 4}]
 
-    var d3 = {
-      yy:       [1, 1, 3, 1,  3, 1, 3, 3],
-      xx:       [3, 1, 2, 3,  4, 3, 2, 4],
-      pathData: [{shapeId: 0, size: 4}, {shapeId: 1, size: 4}]
-    };
+      var out = buildPathTopology(xx, yy, pathData);
+      assert.deepEqual(out.arcs,
+        // [c, a, b] [b, c] [b, d, c]
+        [[[3, 1, 2], [1, 1, 3]], [[2, 3], [3, 1]], [[2, 4, 3], [3, 3, 1]]]);
+      assert.deepEqual(out.paths, [[0, 1], [-2, 2]]);
+      assert.deepEqual(out.sharedArcFlags, [0, 1, 0]);
+    })
 
+    it('ring endpoints are misaligned 1', function () {
+          //   [c, a, b, c] [b, d, c, b]
+      var yy = [1, 1, 3, 1,  3, 3, 1, 3],
+          xx = [3, 1, 2, 3,  2, 4, 3, 2],
+          pathData = [{isHole: false, size: 4}, {isHole: false, size: 4}]
 
-    var e1 = {
-      yy:       [1, 1, 3, 1,  1, 3, 3, 2],
-      xx:       [3, 1, 2, 3,  3, 2, 4, 3], // second ring not closed
-      pathData: [{shapeId: 0, size: 4}, {shapeId: 1, size: 4}]
-    };
+      var out = buildPathTopology(xx, yy, pathData);
+      assert.deepEqual(out.arcs,
+        // [c, a, b] [b, c] [b, d, c]
+        [[[3, 1, 2], [1, 1, 3]], [[2, 3], [3, 1]], [[2, 4, 3], [3, 3, 1]]]);
+      assert.deepEqual(out.paths, [[0, 1], [2, -2]]);
+      assert.deepEqual(out.sharedArcFlags, [0, 1, 0]);
+    })
 
-    /*
-    --    --
-   |  |  |  |
-   *--x--x--*
-   |        |
-    --------
-    */
+    it('ring endpoints are misaligned 2', function () {
+          //   [c, a, b, c] [d, c, b, c]
+      var yy = [1, 1, 3, 1,  3, 1, 3, 3],
+          xx = [3, 1, 2, 3,  4, 3, 2, 4],
+          pathData = [{isHole: false, size: 4}, {isHole: false, size: 4}]
 
-    it("d1 should make three arcs", function() {
-      var data = api.buildTopology(d1);
-      assert.equal(data.arcs.length, 3);
-      assert.deepEqual(data.arcs, [
-        [[3, 1, 2], [1, 1, 3]],
-        [[2, 3], [3, 1]],
-        [[2, 4, 3], [3, 3, 1]]
-        ]);
-      assert.deepEqual(data.shapes, [[[0, 1]], [[2, -2]]]);
-      assert.deepEqual(data.sharedArcFlags, [0, 1, 0]);
-    });
-
-    it("d2 should make three arcs", function() {
-      var data = api.buildTopology(d2);
-      assert.equal(data.arcs.length, 3);
-    });
-
-    it("d3 should make three arcs", function() {
-      var data = api.buildTopology(d3);
-      assert.equal(data.arcs.length, 3);
-    });
-
-
+      var out = buildPathTopology(xx, yy, pathData);
+      assert.deepEqual(out.arcs,
+        // [c, a, b] [b, c] [b, d, c]
+        [[[3, 1, 2], [1, 1, 3]], [[2, 3], [3, 1]], [[2, 4, 3], [3, 3, 1]]]);
+      assert.deepEqual(out.paths, [[0, 1], [-2, 2]]);
+      assert.deepEqual(out.sharedArcFlags, [0, 1, 0]);
+    })
   })
 
 })
