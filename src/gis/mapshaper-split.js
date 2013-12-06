8f4508090e984c8d89cd8618b54e065d03485b7a
Matthew Bloch <masiyou@gmail.com>
2013-12-06 10:23:02 -0500

    Implement splitting on a field

--- src/gis/mapshaper-split.js
+++ src/gis/mapshaper-split.js
@@ -1,5 +1,55 @@
 /* @requires mapshaper-shapes */
 
+MapShaper.splitLayersOnField = function(layers, arcs, field) {
+  var splitLayers = [];
+  Utils.forEach(layers, function(lyr) {
+    splitLayers = splitLayers.concat(MapShaper.splitOnField(lyr, arcs, field));
+  });
+  return splitLayers;
+};
+
+MapShaper.splitOnField = function(lyr0, arcs, field) {
+  var dataTable = lyr0.data;
+  if (!dataTable) error("[splitOnField] Missing a data table");
+  if (!dataTable.fieldExists(field)) error("[splitOnField] Missing field:", field);
+
+  var index = {},
+      properties = dataTable.getRecords(),
+      shapes = lyr0.shapes,
+      splitLayers = [];
+
+  Utils.forEach(shapes, function(shp, i) {
+    var rec = properties[i],
+        key = rec[field],
+        lyr, idx;
+
+    if (key in index === false) {
+      idx = splitLayers.length;
+      index[key] = idx;
+      splitLayers.push({
+        name: key || Utils.getUniqueName("layer"),
+        properties: [],
+        shapes: []
+      });
+    } else {
+      idx = index[key];
+    }
+
+    lyr = splitLayers[idx];
+    lyr.shapes.push(shapes[i]);
+    lyr.properties.push(properties[i]);
+  });
+
+  return Utils.map(splitLayers, function(obj) {
+    return Opts.copyNewParams({
+      name: obj.name,
+      shapes: obj.shapes,
+      data: new DataTable(obj.properties)
+    }, lyr0);
+  });
+};
+
+
 // Split the shapes in a layer according to a grid
 // Return array of layers and an index with the bounding box of each cell
 //
