2c9e735c380c12a85e6a4cf204addf307b28edcf
Matthew Bloch <masiyou@gmail.com>
2014-10-04 03:50:43 -0400

    Add test

---
+++ test/segments-test.js
@@ -0,0 +1,24 @@
+var assert = require('assert'),
+    api = require("../"),
+    internal = api.internal;
+
+describe('mapshaper-segments.js', function () {
+
+  describe('calcSegmentIntersectionStripeCount()', function () {
+    it('Issue #49 test 1', function () {
+      // collapsed islands
+      var arcs = new internal.ArcCollection([
+        [ [ -7162552.387146705, 731171.1486128338 ],
+          [ -7162552.387146705, 731171.1486128338 ] ],
+        [ [ -7152552.387146709, 736171.1486128359 ],
+          [ -7152552.387146709, 736171.1486128359 ] ],
+        [ [ -7152552.387146709, 736171.1486128359 ],
+          [ -7152552.387146709, 736171.1486128359 ] ],
+        [ [ -7156203.834442849, 758887.8997114667 ],
+          [ -7156203.834442849, 758887.8997114667 ] ] ]);
+
+      assert.equal(internal.calcSegmentIntersectionStripeCount(arcs), 1);
+    })
+  })
+
+})
