dc51747a035b5f3e79f5c4d7634da31dc4e0e90a
Matthew Bloch <masiyou@gmail.com>
2015-06-19 12:19:24 -0400

    Revert wrapper div around map to fix resize jitters

--- www/mapshaper.js
+++ www/mapshaper.js
@@ -13175,7 +13175,7 @@ function Editor() {
     exporter = new ExportControl();
     repair = new RepairControl(map);
     slider = new SimplifyControl();
-    El("#mshp-main-map").show();
+    El("#mshp-main-page").show();
   }
 
   function editDataset(dataset, opts) {
