77de7d78ce0d92b050ea351ee4d94c17f6914e14
Matthew Bloch <masiyou@gmail.com>
2015-08-29 00:51:28 -0400

    Add new topology files

---
+++ src/topology/mapshaper-topology-chains-v1.js
@@ -0,0 +1,46 @@
+/* @requires mapshaper-hash-function */
+
+// Return an array with data for chains of vertices with same x, y coordinates
+// Array contains ids of next point in each chain.
+// Unique vertices link to themselves (i.e. arr[n] == n)
+//
+function initPointChains(xx, yy) {
+  var pointCount = xx.length,
+      // Performance doesn't improve much above ~1.3 * point count
+      hashTableSize = Math.floor(pointCount * 1.4),
+      hash = getXYHash(hashTableSize),
+      // Hash table is temporary storage for building chains of coincident points.
+      // Hash bins contain the id of the first point in a chain.
+      hashTable = new Int32Array(hashTableSize),
+      chainIds = new Int32Array(pointCount), // Array to be filled with chain data
+      key, headId, x, y;
+
+  utils.initializeArray(hashTable, -1);
+
+  for (var i=0; i<pointCount; i++) {
+    x = xx[i];
+    y = yy[i];
+    key = hash(x, y);
+    // key = hash2(x, y, hashTableSize);
+
+    while (true) {
+      headId = hashTable[key];
+      if (headId == -1) {
+        // case -- first coordinate in chain: start new chain, point to self
+        hashTable[key] = i;
+        chainIds[i] = i;
+        break;
+      }
+      if (xx[headId] == x && yy[headId] == y) {
+        // case -- extending a chain: insert new point after head of chain
+        chainIds[i] = chainIds[headId];
+        chainIds[headId] = i;
+        break;
+      }
+      // Current hash location is taken by a different point;
+      // try the next location (linear probing).
+      key = (key + 1) % hashTableSize;
+    }
+  }
+  return chainIds;
+}
---
+++ src/topology/mapshaper-topology-chains-v2.js
@@ -0,0 +1,51 @@
+/* @requires mapshaper-hash-function */
+
+function initHashChains(xx, yy) {
+  // Performance doesn't improve much above ~1.3 * point count
+  var n = xx.length,
+      m = Math.floor(n * 1.3) || 1,
+      hash = getXYHash(m),
+      hashTable = new Int32Array(m),
+      chainIds = new Int32Array(n), // Array to be filled with chain data
+      key, j;
+
+  for (var i=0; i<n; i++) {
+    key = hash(xx[i], yy[i]);
+    j = hashTable[key] - 1; // coord ids are 1-based in hash table; 0 used as null value.
+    hashTable[key] = i + 1;
+    chainIds[i] = j < 0 ? i : j; // first item in a chain points to self
+  }
+  return chainIds;
+}
+
+function initPointChains(xx, yy) {
+  var chainIds = initHashChains(xx, yy),
+      j, next, prevMatchId, prevUnmatchId;
+
+  // disentangle, reverse and close the chains created by initHashChains()
+  for (var i = xx.length-1; i>=0; i--) {
+    next = chainIds[i];
+    if (next >= i) continue;
+    prevMatchId = i;
+    prevUnmatchId = -1;
+    do {
+      j = next;
+      next = chainIds[j];
+      if (yy[j] == yy[i] && xx[j] == xx[i]) {
+        chainIds[j] = prevMatchId;
+        prevMatchId = j;
+      } else {
+        if (prevUnmatchId > -1) {
+          chainIds[prevUnmatchId] = j;
+        }
+        prevUnmatchId = j;
+      }
+    } while (next < j);
+    if (prevUnmatchId > -1) {
+      // Make sure last unmatched entry is terminated
+      chainIds[prevUnmatchId] = prevUnmatchId;
+    }
+    chainIds[i] = prevMatchId; // close the chain
+  }
+  return chainIds;
+}
