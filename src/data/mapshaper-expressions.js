89e0eb3cefdd6d62b56ed6bd8be206e5b73dc691
Matthew Bloch <masiyou@gmail.com>
2016-11-12 12:00:40 -0500

    Refactor expressions, use 'this' as alternative to '$'

--- src/data/mapshaper-expressions.js
+++ src/data/mapshaper-expressions.js
@@ -1,9 +1,4 @@
-/* @requires
-mapshaper-shape-geom
-mapshaper-polygon-centroid
-mapshaper-arcs
-mapshaper-dataset-utils
-*/
+/* @requires mapshaper-feature-proxy */
 
 // Compiled expression returns a value
 MapShaper.compileValueExpression = function(exp, lyr, arcs) {
@@ -42,20 +37,21 @@ MapShaper.getAssignedVars = function(exp) {
 };
 
 MapShaper.getExpressionFunction = function(exp, lyr, arcs, returns) {
-  var env = MapShaper.getExpressionContext(lyr, arcs);
-  var body = (returns ? 'return ' : '') + exp;
+  var getFeatureById = MapShaper.initFeatureProxy(lyr, arcs);
+  var ctx = MapShaper.getExpressionContext(lyr);
+  var functionBody = "with(env){with(record){ " + (returns ? 'return ' : '') +
+        exp + "}}";
   var func;
   try {
-    func = new Function("record,env", "with(env){with(record){ " + body + "}}");
+    func = new Function("record,env",  functionBody);
   } catch(e) {
     stop(e.name, "in expression [" + exp + "]");
   }
-
   return function(rec, i) {
     var val;
-    env.$.__setId(i);
+    ctx.$ = getFeatureById(i);
     try {
-      val = func.call(null, rec, env);
+      val = func.call(ctx.$, rec, ctx);
     } catch(e) {
       stop(e.name, "in expression [" + exp + "]:", e.message);
     }
@@ -63,7 +59,7 @@ MapShaper.getExpressionFunction = function(exp, lyr, arcs, returns) {
   };
 };
 
-MapShaper.getExpressionContext = function(lyr, arcs) {
+MapShaper.getExpressionContext = function(lyr) {
   var env = MapShaper.getBaseContext();
   if (lyr.data) {
     // default to null values when a data field is missing
@@ -71,7 +67,6 @@ MapShaper.getExpressionContext = function(lyr, arcs) {
       env[f] = null;
     });
   }
-  env.$ = new FeatureExpressionContext(lyr, arcs);
   return env;
 };
 
@@ -86,161 +81,3 @@ MapShaper.getBaseContext = function() {
   obj.console = console;
   return obj;
 };
-
-
-function addGetters(obj, getters) {
-  Object.keys(getters).forEach(function(name) {
-    Object.defineProperty(obj, name, {get: getters[name]});
-  });
-}
-
-function FeatureExpressionContext(lyr, arcs) {
-  var hasData = !!lyr.data,
-      hasPoints = MapShaper.layerHasPoints(lyr),
-      hasPaths = arcs && MapShaper.layerHasPaths(lyr),
-      _isPlanar,
-      _self = this,
-      _centroid, _innerXY, _xy,
-      _record, _records,
-      _id, _ids, _bounds;
-
-  if (hasData) {
-    _records = lyr.data.getRecords();
-    Object.defineProperty(this, 'properties',
-      {set: function(obj) {
-        if (utils.isObject(obj)) {
-          _records[_id] = obj;
-        } else {
-          stop("Can't assign non-object to $.properties");
-        }
-      }, get: function() {
-        var rec = _records[_id];
-        if (!rec) {
-          rec = _records[_id] = {};
-        }
-        return rec;
-      }});
-  }
-
-  if (hasPaths) {
-    _isPlanar = arcs.isPlanar();
-    addGetters(this, {
-      // TODO: count hole/s + containing ring as one part
-      partCount: function() {
-        return _ids ? _ids.length : 0;
-      },
-      isNull: function() {
-        return this.partCount === 0;
-      },
-      bounds: function() {
-        return shapeBounds().toArray();
-      },
-      height: function() {
-        return shapeBounds().height();
-      },
-      width: function() {
-        return shapeBounds().width();
-      }
-    });
-
-    if (lyr.geometry_type == 'polygon') {
-      addGetters(this, {
-        area: function() {
-          return _isPlanar ? geom.getPlanarShapeArea(_ids, arcs) : geom.getSphericalShapeArea(_ids, arcs);
-        },
-        originalArea: function() {
-          var i = arcs.getRetainedInterval(),
-              area;
-          arcs.setRetainedInterval(0);
-          area = _self.area;
-          arcs.setRetainedInterval(i);
-          return area;
-        },
-        centroidX: function() {
-          var p = centroid();
-          return p ? p.x : null;
-        },
-        centroidY: function() {
-          var p = centroid();
-          return p ? p.y : null;
-        },
-        innerX: function() {
-          var p = innerXY();
-          return p ? p.x : null;
-        },
-        innerY: function() {
-          var p = innerXY();
-          return p ? p.y : null;
-        }
-      });
-    }
-
-  } else if (hasPoints) {
-    // TODO: add functions like bounds, isNull, pointCount
-    Object.defineProperty(this, 'coordinates',
-      {set: function(obj) {
-        if (!obj || utils.isArray(obj)) {
-          lyr.shapes[_id] = obj || null;
-        } else {
-          stop("Can't assign non-array to $.coordinates");
-        }
-      }, get: function() {
-        return lyr.shapes[_id] || null;
-      }});
-
-    addGetters(this, {
-      x: function() {
-        xy();
-        return _xy ? _xy[0] : null;
-      },
-      y: function() {
-        xy();
-        return _xy ? _xy[1] : null;
-      }
-    });
-  }
-
-  // all contexts have $.id
-  addGetters(this, {id: function() { return _id; }});
-
-  this.__setId = function(id) {
-    _id = id;
-    if (hasPaths) {
-      _bounds = null;
-      _centroid = null;
-      _innerXY = null;
-      _ids = lyr.shapes[id];
-    }
-    if (hasPoints) {
-      _xy = null;
-    }
-    if (hasData) {
-      _record = _records[id];
-    }
-  };
-
-  function xy() {
-    var shape = lyr.shapes[_id];
-    if (!_xy) {
-      _xy = shape && shape[0] || null;
-    }
-    return _xy;
-  }
-
-  function centroid() {
-    _centroid = _centroid || geom.getShapeCentroid(_ids, arcs);
-    return _centroid;
-  }
-
-  function innerXY() {
-    _innerXY = _innerXY || geom.findInteriorPoint(_ids, arcs);
-    return _innerXY;
-  }
-
-  function shapeBounds() {
-    if (!_bounds) {
-      _bounds = arcs.getMultiShapeBounds(_ids);
-    }
-    return _bounds;
-  }
-}
---
+++ src/data/mapshaper-feature-proxy.js
@@ -0,0 +1,158 @@
+/* @requires
+mapshaper-shape-geom
+mapshaper-polygon-centroid
+mapshaper-arcs
+mapshaper-dataset-utils
+*/
+
+function addGetters(obj, getters) {
+  Object.keys(getters).forEach(function(name) {
+    Object.defineProperty(obj, name, {get: getters[name]});
+  });
+}
+
+MapShaper.initFeatureProxy = function(lyr, arcs) {
+  var hasPoints = MapShaper.layerHasPoints(lyr),
+      hasPaths = arcs && MapShaper.layerHasPaths(lyr),
+      _records = lyr.data ? lyr.data.getRecords() : null,
+      _isPlanar = hasPaths && arcs.isPlanar(),
+      ctx = {},
+      _bounds, _centroid, _innerXY, _xy, _ids, _id;
+
+  // all contexts have $.id
+  addGetters(ctx, {id: function() { return _id; }});
+
+  if (_records) {
+    Object.defineProperty(ctx, 'properties',
+      {set: function(obj) {
+        if (utils.isObject(obj)) {
+          _records[_id] = obj;
+        } else {
+          stop("Can't assign non-object to $.properties");
+        }
+      }, get: function() {
+        var rec = _records[_id];
+        if (!rec) {
+          rec = _records[_id] = {};
+        }
+        return rec;
+      }});
+  }
+
+  if (hasPaths) {
+    addGetters(ctx, {
+      // TODO: count hole/s + containing ring as one part
+      partCount: function() {
+        return _ids ? _ids.length : 0;
+      },
+      isNull: function() {
+        return ctx.partCount === 0;
+      },
+      bounds: function() {
+        return shapeBounds().toArray();
+      },
+      height: function() {
+        return shapeBounds().height();
+      },
+      width: function() {
+        return shapeBounds().width();
+      }
+    });
+
+    if (lyr.geometry_type == 'polygon') {
+      addGetters(ctx, {
+        area: function() {
+          return _isPlanar ? geom.getPlanarShapeArea(_ids, arcs) : geom.getSphericalShapeArea(_ids, arcs);
+        },
+        originalArea: function() {
+          var i = arcs.getRetainedInterval(),
+              area;
+          arcs.setRetainedInterval(0);
+          area = _self.area;
+          arcs.setRetainedInterval(i);
+          return area;
+        },
+        centroidX: function() {
+          var p = centroid();
+          return p ? p.x : null;
+        },
+        centroidY: function() {
+          var p = centroid();
+          return p ? p.y : null;
+        },
+        innerX: function() {
+          var p = innerXY();
+          return p ? p.x : null;
+        },
+        innerY: function() {
+          var p = innerXY();
+          return p ? p.y : null;
+        }
+      });
+    }
+
+  } else if (hasPoints) {
+    // TODO: add functions like bounds, isNull, pointCount
+    Object.defineProperty(ctx, 'coordinates',
+      {set: function(obj) {
+        if (!obj || utils.isArray(obj)) {
+          lyr.shapes[_id] = obj || null;
+        } else {
+          stop("Can't assign non-array to $.coordinates");
+        }
+      }, get: function() {
+        return lyr.shapes[_id] || null;
+      }});
+
+    addGetters(ctx, {
+      x: function() {
+        xy();
+        return _xy ? _xy[0] : null;
+      },
+      y: function() {
+        xy();
+        return _xy ? _xy[1] : null;
+      }
+    });
+  }
+
+  function xy() {
+    var shape = lyr.shapes[_id];
+    if (!_xy) {
+      _xy = shape && shape[0] || null;
+    }
+    return _xy;
+  }
+
+  function centroid() {
+    _centroid = _centroid || geom.getShapeCentroid(_ids, arcs);
+    return _centroid;
+  }
+
+  function innerXY() {
+    _innerXY = _innerXY || geom.findInteriorPoint(_ids, arcs);
+    return _innerXY;
+  }
+
+  function shapeBounds() {
+    if (!_bounds) {
+      _bounds = arcs.getMultiShapeBounds(_ids);
+    }
+    return _bounds;
+  }
+
+  return function(id) {
+    _id = id;
+    // reset stored values
+    if (hasPaths) {
+      _bounds = null;
+      _centroid = null;
+      _innerXY = null;
+      _ids = lyr.shapes[id];
+    }
+    if (hasPoints) {
+      _xy = null;
+    }
+    return ctx;
+  };
+};
