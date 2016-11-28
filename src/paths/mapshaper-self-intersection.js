5b42dfbe4aa953faa431ec3e7da0c7f27682bda0
Matthew Bloch <masiyou@gmail.com>
2016-11-28 14:51:29 -0500

    Refactor self-intersection splitter

--- src/paths/mapshaper-self-intersection.js
+++ src/paths/mapshaper-self-intersection.js
@@ -1,52 +1,14 @@
 /* @requires mapshaper-common */
 
 // Return function for splitting self-intersecting polygon rings
-// Returned function receives a single path, returns an array of paths
-// Assumes that any intersections occur at vertices, not along segments
+// Splitter function receives a single path, returns an array of paths
+// Intersections are assumed to occur at vertices, not along segments
 // (requires that MapShaper.addIntersectionCuts() has already been run)
 //
 MapShaper.getSelfIntersectionSplitter = function(nodes) {
+  return dividePath;
 
-  function contains(arr, el) {
-    for (var i=0, n=arr.length; i<n; i++) {
-      if (arr[i] === el) return true;
-    }
-    return false;
-  }
-
-  // If arc @enterId enters a node with more than one open routes leading out:
-  //   return array of sub-paths
-  // else return null
-  function dividePathAtNode(path, enterId) {
-    var count = 0,
-        subPaths = null,
-        exitIds, firstExitId;
-    nodes.forEachConnectedArc(enterId, function(arcId) {
-      var exitId = ~arcId;
-      // TODO: remove performance bottleneck
-      // contains() is faster than native array.indexOf(), could do better.
-      // if (path.indexOf(exitId) > -1) { // ignore arcs that are not on this path
-      if (contains(path, exitId)) { // ignore arcs that are not on this path
-        if (count === 0) {
-          firstExitId = exitId;
-        } else if (count === 1) {
-          exitIds = [firstExitId, exitId];
-        } else {
-          exitIds.push(exitId);
-        }
-        count++;
-      }
-    });
-    if (exitIds) {
-      subPaths = MapShaper.splitPathByIds(path, exitIds);
-      // recursively divide each sub-path
-      return subPaths.reduce(function(memo, subPath) {
-        return memo.concat(dividePath(subPath));
-      }, []);
-    }
-    return null;
-  }
-
+  // Returns array of 0 or more divided paths
   function dividePath(path) {
     var subPaths = null;
     for (var i=0; i<path.length - 1; i++) { // don't need to check last arc
@@ -55,41 +17,71 @@ MapShaper.getSelfIntersectionSplitter = function(nodes) {
         return subPaths;
       }
     }
-    // indivisible path -- remove any spikes
+    // indivisible path -- clean it by removing any spikes
     MapShaper.removeSpikesInPath(path);
     return path.length > 0 ? [path] : [];
   }
 
-  return dividePath;
+  // If arc @enterId enters a node with more than one open routes leading out:
+  //   return array of sub-paths
+  // else return null
+  function dividePathAtNode(path, enterId) {
+    var nodeIds = nodes.getConnectedArcs(enterId),
+        exitIds = [],
+        outId;
+    for (var i=0; i<nodeIds.length; i++) {
+      outId = ~nodeIds[i];
+      if (contains(path, outId)) { // repeated scanning may be bottleneck
+        exitIds.push(outId);
+      }
+    }
+    if (exitIds.length > 1) {
+      // path forks -- recursively subdivide
+      return MapShaper.splitPathByIds(path, exitIds).reduce(accumulatePaths, null);
+    }
+    return null;
+  }
+
+  function accumulatePaths(memo, path) {
+    var subPaths = dividePath(path);
+    return memo ? memo.concat(subPaths) : subPaths;
+  }
+
+  // Added as an optimization -- tested faster than using Array#indexOf()
+  function contains(arr, el) {
+    for (var i=0, n=arr.length; i<n; i++) {
+      if (arr[i] === el) return true;
+    }
+    return false;
+  }
 };
 
-// @path An array of arc ids
-// @ids An array of two or more start ids
+// Function returns an array of split-apart rings
+// @path An array of arc ids describing a self-intersecting polygon ring
+// @ids An array of two or more ids of arcs that originate from a single vertex
+//      where @path intersects itself.
 MapShaper.splitPathByIds = function(path, ids) {
-  var n = ids.length;
-  var ii = ids.map(function(id) {
-    var idx = path.indexOf(id);
-    if (idx == -1) error("[splitPathByIds()] Path is missing id:", id);
-    return idx;
+  var subPaths = [];
+  // Find array indexes in @path of each split id
+  var indexes = ids.map(function(id) {
+    var i = path.indexOf(id);
+    if (i == -1) error("[splitPathByIds()] missing arc:", id);
+    return i;
   });
-  utils.genericSort(ii, true);
-  var subPaths = ii.map(function(idx, i) {
-    var split;
-    if (i == n-1) {
-      // place first path item first
-      split = path.slice(0, ii[0]).concat(path.slice(idx));
-    } else {
-      split = path.slice(idx, ii[i+1]);
-    }
-    return split;
-  });
-
-  // make sure first sub-path starts with arc at path[0]
-  if (ii[0] !== 0) {
-    subPaths.unshift(subPaths.pop());
+  utils.genericSort(indexes, true); // sort ascending
+  if (indexes[0] > 0) {
+    subPaths.push(path.slice(0, indexes[0]));
   }
-  if (subPaths[0][0] !== path[0]) {
-    error("[splitPathByIds()] Indexing error");
+  for (var i=0, n=indexes.length; i<n; i++) {
+    if (i < n-1) {
+      subPaths.push(path.slice(indexes[i], indexes[i+1]));
+    } else {
+      subPaths.push(path.slice(indexes[i]));
+    }
+  }
+  // handle case where first subring is split across endpoint of @path
+  if (subPaths.length > ids.length) {
+    subPaths[0] = subPaths[0].concat(subPaths.pop());
   }
   return subPaths;
 };
