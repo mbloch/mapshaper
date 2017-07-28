ff3cf8126890bcc16a33f6002c42af4d565cf0b4
Matthew Bloch <masiyou@gmail.com>
2017-07-28 18:43:40 -0400

    Update website

--- mapshaper.js
+++ mapshaper.js
@@ -1,5 +1,5 @@
 (function(){
-var VERSION = '0.4.38';
+var VERSION = '0.4.39';
 
 var error = function() {
   var msg = Utils.toArray(arguments).join(' ');
@@ -12939,6 +12939,15 @@ internal.projectionIndex = {
   albersusa: AlbersNYT
 };
 
+// This stub is replaced when loaded in GUI, which may need to load some files
+internal.initProjLibrary = function(opts, done) {done();};
+
+// Find Proj.4 definition file names in strings like "+init=epsg:3000"
+// (Used by GUI, defined here for testing)
+internal.findProjLibs = function(str) {
+  return utils.uniq(str.match(/\b(esri|epsg|nad83|nad27)(?=:[0-9]+\b)/g) || []);
+};
+
 internal.getProjInfo = function(dataset) {
   var P, info;
   try {
@@ -17598,18 +17607,27 @@ api.runCommand = function(cmd, catalog, cb) {
       outputLayers = internal.applyCommand(api.createPointLayer, targetLayers, arcs, opts);
 
     } else if (name == 'proj') {
-      targets.forEach(function(targ) {
-        var srcInfo, destInfo;
-        if (opts.from) {
-          srcInfo = internal.getProjectionInfo(opts.from, catalog);
-          if (!srcInfo.crs) stop("Unknown projection source:", opts.from);
-          internal.setDatasetProjection(targ.dataset, srcInfo);
+      internal.initProjLibrary(opts, function() {
+        var err = null;
+        try {
+          targets.forEach(function(targ) {
+            var srcInfo, destInfo;
+            if (opts.from) {
+              srcInfo = internal.getProjectionInfo(opts.from, catalog);
+              if (!srcInfo.crs) stop("Unknown projection source:", opts.from);
+              internal.setDatasetProjection(targ.dataset, srcInfo);
+            }
+            if (opts.match || opts.projection) {
+              destInfo = internal.getProjectionInfo(opts.match || opts.projection, catalog);
+              api.proj(targ.dataset, destInfo, opts);
+            }
+          });
+        } catch(e) {
+          err = e;
         }
-        if (opts.match || opts.projection) {
-          destInfo = internal.getProjectionInfo(opts.match || opts.projection, catalog);
-          api.proj(targ.dataset, destInfo, opts);
-        }
+        done(err);
       });
+      return; // async command
 
     } else if (name == 'rename-fields') {
       internal.applyCommand(api.renameFields, targetLayers, opts.fields);
