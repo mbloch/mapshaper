5c9c0e65bc1917a4cc015a1ba1180a58b1cecb31
Matthew Bloch <masiyou@gmail.com>
2017-11-11 10:20:21 -0500

    Tests for filter-geom bbox=

---
+++ test/filter-geom-test.js
@@ -0,0 +1,71 @@
+
+var api = require('..'),
+    assert = require('assert');
+
+describe('mapshaper-filter-geom.js', function () {
+
+  describe('bbox= option', function () {
+
+    it('error: missing geometry', function(done) {
+      var data = [{foo: "bar"}];
+      api.applyCommands('-i data.json -filter-geom bbox=0,0,1,1 -o', {'data.json':data}, function(err, out) {
+        assert.equal(err.name, 'UserError');
+        done();
+      })
+    })
+
+    it('point layer', function (done) {
+      var points = {
+        type: 'GeometryCollection',
+        geometries: [{
+          type: 'MultiPoint',
+          coordinates: [[0, 0], [180, 0], [180, -1], [-180, 90]]
+        }, {
+          type: 'Point',
+          coordinates: [10, -10]
+        }]
+      };
+      var expected = {
+        type: 'GeometryCollection',
+        geometries: [{
+          type: 'MultiPoint',
+          coordinates: [[0, 0], [180, 0], [-180, 90]]
+        }]
+      };
+      var cmd = '-i points.json -filter-geom bbox=-180,0,180,90 -o';
+      api.applyCommands(cmd, {'points.json': points}, function(err, out) {
+        var result = JSON.parse(out['points.json']);
+        assert.deepEqual(result, expected)
+        done();
+      });
+    })
+
+    it('polyline layer', function (done) {
+      var lines = {
+        type: 'GeometryCollection',
+        geometries: [{
+          type: 'MultiLineString',
+          coordinates: [[[0, 0], [0, -10]], [[180, -1], [179, -2]], [[-180, 90], [-180, 89]]]
+        }, {
+          type: 'LineString',
+          coordinates: [[10, -10], [11, -11]]
+        }]
+      };
+      var expected = {
+        type: 'GeometryCollection',
+        geometries: [{
+          type: 'MultiLineString',
+          coordinates: [[[0, 0], [0, -10]], [[-180, 90], [-180, 89]]]
+        }]
+      };
+      var cmd = '-i lines.json -filter-geom bbox=-180,0,180,90 -o';
+      api.applyCommands(cmd, {'lines.json': lines}, function(err, out) {
+        var result = JSON.parse(out['lines.json']);
+        assert.deepEqual(result, expected)
+        done();
+      });
+    });
+
+  })
+
+})
