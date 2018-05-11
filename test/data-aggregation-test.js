4d27e7b0a9bb03a6c3c8183f9b1ee95c87b5bde4
Matthew Bloch <masiyou@gmail.com>
2018-05-11 12:54:39 -0400

    Add data-aggregation-test.js

---
+++ test/data-aggregation-test.js
@@ -0,0 +1,19 @@
+var api = require('../'),
+    internal = api.internal,
+    assert = require('assert');
+
+describe('mapshaper-data-aggregation.js', function () {
+  describe('getMultiFieldKeyFunction()', function () {
+    var f = internal.getMultiFieldKeyFunction;
+    it('single keys', function () {
+      assert.equal(f(['foo'])({foo: 'bar'}), 'bar');
+      assert.equal(f(['foo'])({foo: 100.23}), '100.23');
+      assert.equal(f(['foo'])({foo: true}), 'true');
+      assert.equal(f(['foo'])({}), 'undefined');
+    })
+    it('multiple keys', function () {
+      assert.equal(f(['foo','bar','baz'])({foo:'a', bar:0, baz:null}), 'a~~0~~null');
+    })
+  })
+
+})
