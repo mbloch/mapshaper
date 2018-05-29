397c3b7b413a2769a570b624c03cdf1ead67fd03
Matthew Bloch <masiyou@gmail.com>
2018-05-29 18:21:50 -0400

    Another shot at supporting v4.x

--- src/utils/mapshaper-buffer.js
+++ src/utils/mapshaper-buffer.js
@@ -5,6 +5,7 @@ utils.createBuffer = function(arg, arg2) {
   if (utils.isInteger(arg)) {
     return Buffer.allocUnsafe ? Buffer.allocUnsafe(arg) : new Buffer(arg);
   } else {
-    return Buffer.from ? Buffer.from(arg, arg2) : new Buffer(arg);
+    // check allocUnsafe to make sure Buffer.from() will accept strings (it didn't before Node v5.10)
+    return Buffer.from && Buffer.allocUnsafe ? Buffer.from(arg, arg2) : new Buffer(arg, arg2);
   }
 };
