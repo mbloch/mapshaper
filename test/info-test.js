0f3d0f9b043800e2ee22100b643d6c7e9e2aed78
Matthew Bloch <masiyou@gmail.com>
2019-03-18 15:16:48 -0400

    Change -info command display

--- test/info-test.js
+++ test/info-test.js
@@ -2,31 +2,31 @@ var assert = require('assert'),
     api = require("../");
 
 describe('mapshaper-info.js', function () {
-  describe('formatTableItem()', function () {
+  describe('formatTableValue()', function () {
 
     it('string field', function() {
-      var target = "foo  'bar'";
-      assert.equal(api.internal.formatTableItem('foo', 'bar', 5, 0), target);
+      var target = "'bar'";
+      assert.equal(api.internal.formatTableValue('bar', 0), target);
     });
 
     it('decimal field', function() {
-      var target = 'foo   -0.4';
-      assert.equal(api.internal.formatTableItem('foo', -0.4, 4, 4), target);
+      var target = '  -0.4';
+      assert.equal(api.internal.formatTableValue(-0.4, 4), target);
     });
 
     it('integer field', function() {
-      var target = 'foo 333';
-      assert.equal(api.internal.formatTableItem('foo', 333, 4, 3), target);
+      var target = '333';
+      assert.equal(api.internal.formatTableValue(333, 3), target);
     });
 
     it('object field', function() {
-      var target = 'foo {"a":2}';
-      assert.equal(api.internal.formatTableItem('foo', {a: 2}, 4, 0), target);
+      var target = '{"a":2}';
+      assert.equal(api.internal.formatTableValue({a: 2}, 0), target);
     });
 
     it('null value', function() {
-      var target = 'foo  null';
-      assert.equal(api.internal.formatTableItem('foo', null, 5, 0), target);
+      var target = 'null';
+      assert.equal(api.internal.formatTableValue(null, 0), target);
     });
 
   })
