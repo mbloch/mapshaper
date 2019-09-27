5f539aaecff70873df0d0361ff4b94707cb4cbdb
Matthew Bloch <masiyou@gmail.com>
2019-09-27 15:36:49 -0400

    Update website

--- mapshaper.js
+++ mapshaper.js
@@ -1,5 +1,5 @@
 (function(){
-VERSION = '0.4.130';
+VERSION = '0.4.131';
 
 var error = function() {
   var msg = utils.toArray(arguments).join(' ');
@@ -1388,7 +1388,7 @@ utils.expandoBuffer = function(constructor, rate) {
   return function(size) {
     if (size > capacity) {
       capacity = Math.ceil(size * k);
-      buf = new constructor(capacity);
+      buf = constructor ? new constructor(capacity) : utils.createBuffer(capacity);
     }
     return buf;
   };
@@ -3671,8 +3671,9 @@ Matrix2D.prototype.scale = function(sx, sy, cx, cy) {
 
 // A compound projection, consisting of a default projection and one or more rectangular frames
 // that are projected separately and affine transformed.
-// @projStr Default projection
-// @bbox  lat-lon bounding box of main projection
+// @mainParams: parameters for main projection, including:
+//    proj: Proj string
+//    bbox: lat-lon bounding box
 function MixedProjection(mainParams, options) {
   var mproj = require('mproj');
   var mainFrame = initFrame(mainParams);
@@ -5071,8 +5072,8 @@ function BufferReader(src) {
 function FileReader(path, opts) {
   var fs = require('fs'),
       fileLen = fs.statSync(path).size,
-      DEFAULT_CACHE_LEN = opts && opts.cacheSize || 0x800000, // 8MB
-      DEFAULT_BUFFER_LEN = opts && opts.bufferSize || 0x4000, // 32K
+      DEFAULT_CACHE_LEN = opts && opts.cacheSize || 0x1000000, // 16MB
+      DEFAULT_BUFFER_LEN = opts && opts.bufferSize || 0x40000, // 256K
       fd, cacheOffs, cache, binArr;
 
   internal.getStateVar('input_files').push(path); // bit of a kludge
@@ -5173,28 +5174,36 @@ FileReader.prototype.findString = function (str, maxLen) {
 
 
 
+// Read and parse a DSV file
+// This function parses delimited text files 500 lines at a time, which
+// avoids reading the file as a single string (which limits the allowable
+// file size to as little as 256MB, depending on the JS engine).
+//
 // TODO: support other encodings than utf-8
 // (Need to update readDelimLines() to work with all encodings)
-internal.readDelimRecords = function(reader, delim, encoding, filter) {
+internal.readDelimRecords = function(reader, delim, optsArg) {
   var dsv = require("d3-dsv").dsvFormat(delim),
+      opts = optsArg || {},
+      encoding = opts.encoding,
+      filter = internal.getImportFilterFunction(opts),
       records = [],
-      retn = internal.readDelimLines(reader, 0, delim, encoding, 1),
+      retn = internal.readDelimLines(reader, 0, encoding, 1),
       header = internal.trimBOM(retn ? retn.text : ''),
+      batchSize = opts.batch_size || 500,
       batch;
   if (!retn) return []; // e.g. empty file
   // read in batches (faster than line-by-line)
-  while ((retn = internal.readDelimLines(reader, retn.offset, delim, encoding, 500))) {
+  while ((retn = internal.readDelimLines(reader, retn.offset, encoding, batchSize))) {
     batch = dsv.parse(header + retn.text, filter);
     records.push.apply(records, batch);
   }
   return records;
 };
 
-internal.readDelimLines = function(reader, offs, delim, encoding, lines) {
+internal.readDelimLines = function(reader, offs, encoding, lines) {
   var CR = 13,
       LF = 10,
       DQUOTE = 34,
-      DELIM = delim.charCodeAt(0),
       inQuotedField = false,
       buf = reader.readSync(offs),
       eol = false,
@@ -5233,6 +5242,145 @@ internal.readDelimLines = function(reader, offs, delim, encoding, lines) {
     text: internal.bufferToString(buf, encoding, 0, i)
   };
 };
+
+// Read and parse a DSV file
+// This version performs field filtering at a low level (before parsing with d3),
+// which improves performance when filtering out many fields from a large file.
+// (tested with a 40GB CSV)
+//
+// TODO: support other encodings than utf-8 and ascii
+// (Need to update readDelimLines() to work with all encodings)
+internal.readDelimRecords2 = function(reader, delim, optsArg) {
+  var dsv = require("d3-dsv").dsvFormat(delim),
+      opts = optsArg || {},
+      records = [],
+      inputBuf = reader.readSync(0),
+      retn = internal.readDelimLines2(inputBuf, 1, delim), // read one line (assumed to contain field names)
+      fullHeader = internal.trimBOM(retn.bytesRead ? internal.decodeString(retn.buffer, opts.encoding) : ''),
+      allFields = dsv.parseRows(fullHeader)[0] || [],
+      rowFilter = opts.csv_filter ? internal.getImportFilterFunction({csv_filter: opts.csv_filter}) : null,
+      colFilter = opts.csv_fields ? internal.getDelimFieldFilter(allFields, opts.csv_fields) : null,
+      headerStr = colFilter ? internal.filterHeaderFields(allFields, colFilter).join(delim) + '\n' : fullHeader,
+      fileOffset = retn.bytesRead,
+      batchSize = opts.batch_size || 1000,
+      batch, dataStr;
+
+  if (!headerStr) return []; // e.g. empty file
+
+  while (fileOffset < reader.size()) {
+    inputBuf = reader.readSync(fileOffset);
+    retn = internal.readDelimLines2(inputBuf, batchSize, delim, colFilter); // read in 1000-line chunks
+    if (retn.bytesRead > 0 === false) error('Error reading file'); // should never happen
+    if (retn.bytesRead >= inputBuf.length && reader.size() > fileOffset + retn.bytesRead) {
+      // if the input buffer overflows, enlarge it and re-read these lines
+      reader.expandBuffer();
+      continue;
+    }
+    dataStr = internal.decodeString(retn.buffer, opts.encoding);
+    batch = dsv.parse(headerStr + dataStr, rowFilter);
+    records.push.apply(records, batch);
+    fileOffset += retn.bytesRead;
+  }
+  return records;
+};
+
+internal.filterHeaderFields = function(fields, colFilter) {
+  return fields.filter(function(name, i) {
+    return colFilter ? colFilter(i) : true;
+  });
+};
+
+// Returns a function for filtering fields by column index
+// The function returns true for retained fields and false for excluded fields
+internal.getDelimFieldFilter = function(header, fieldsToKeep) {
+  var index = utils.arrayToIndex(fieldsToKeep);
+  var map = header.map(function(name) {
+    return name in index;
+  });
+  return function(col) {
+    return map[col];
+  };
+};
+
+internal.readDelimLines2 = (function() {
+  var getOutputBuffer = utils.expandoBuffer();
+
+  function peekNextByte(buf, i) {
+    i++;
+    return i < buf.length ? buf[i] : 0;
+  }
+
+  return function(srcBuf, lines, delim, keepField) {
+    var CR = 13, LF = 10, DQUOTE = 34,
+        DELIM = delim.charCodeAt(0),
+        inQuotedField = false,
+        inExcludedField = false,
+        linesToRead = lines > 0 ? lines : 1,
+        lineCount = 0,
+        bufLen = srcBuf.length,
+        // use a separate buffer for output if filtering fields (data corruption can occur)
+        destBuf = keepField ? getOutputBuffer(bufLen) : srcBuf,
+        i, j, c, keepChar, srcCol, destCol;
+
+    function startLine() {
+      srcCol = -1;
+      destCol = -1;
+      lineCount++;
+      startSourceField();
+    }
+
+    function startSourceField() {
+      srcCol++;
+      if (!keepField || keepField(srcCol)) {
+        inExcludedField = false;
+        destCol++;
+      } else {
+        inExcludedField = true;
+      }
+    }
+
+    startLine();
+    for (i=0, j=0; i < bufLen && lineCount <= linesToRead; i++) {
+      c = srcBuf[i];
+      if (c == DQUOTE) {
+        // according to spec, double quotes either enclose a field or are
+        // paired inside a quoted field
+        // https://tools.ietf.org/html/rfc4180
+        // the following handles both cases (no error checking though)
+        inQuotedField = !inQuotedField;
+      }
+
+      if (inQuotedField) {
+        keepChar = !inExcludedField;
+      } else if (c == CR || c == LF) {
+        keepChar = true;
+        if (c == CR && peekNextByte(srcBuf, i) == LF) {
+          // first half of CRLF pair: don't start a new line yet
+        } else {
+          startLine();
+        }
+      } else if (c == DELIM) {
+        startSourceField();
+        keepChar = !inExcludedField && destCol > 0;
+      } else {
+        keepChar = !inExcludedField;
+      }
+
+      if (keepChar) destBuf[j++] = c;
+    }
+
+    return {
+      bytesRead: i,
+      buffer: destBuf.slice(0, j)
+    };
+  };
+}());
+
+function objectConverter(columns) {
+  return new Function("d", "return {" + columns.map(function(name, i) {
+    return JSON.stringify(name) + ": d[" + i + "] || \"\"";
+  }).join(",") + "}");
+}
 
 
 
@@ -5509,13 +5657,12 @@ internal.importDelim2 = function(data, opts) {
       content = data.content,
       filter, reader, records, delimiter, table;
   opts = opts || {};
-  filter = internal.getImportFilterFunction(opts);
 
-  // read content of all but very large files into a buffer
-  if (readFromFile && cli.fileSize(data.filename) < 2e9) {
-    content = cli.readFile(data.filename);
-    readFromFile = false;
-  }
+  // // read content of all but very large files into a buffer
+  // if (readFromFile && cli.fileSize(data.filename) < 2e9) {
+  //   content = cli.readFile(data.filename);
+  //   readFromFile = false;
+  // }
 
   if (readFromFile) {
     // try to read data incrementally from file, if content is missing
@@ -5539,9 +5686,10 @@ internal.importDelim2 = function(data, opts) {
 
   if (reader) {
     delimiter = internal.guessDelimiter(internal.readFirstChars(reader, 2000));
-    records = internal.readDelimRecords(reader, delimiter, opts.encoding, filter);
+    records = internal.readDelimRecords2(reader, delimiter, opts);
   } else {
     delimiter = internal.guessDelimiter(content);
+    filter = internal.getImportFilterFunction(opts);
     records = require("d3-dsv").dsvFormat(delimiter).parse(content, filter);
     delete records.columns; // added by d3-dsv
   }
@@ -5721,24 +5869,30 @@ utils.parseNumber = function(raw) {
 // Returns a d3-dsv compatible function for filtering records and fields on import
 // TODO: look into using more code from standard expressions.
 internal.getImportFilterFunction = function(opts) {
-  var recordFilter = opts.csv_filter ? internal.compileExpressionToFunction(opts.csv_filter, {returns: true}) : null;
-  var fieldFilter = opts.csv_fields ? internal.getRecordMapper(internal.mapFieldNames(opts.csv_fields)) : null;
+  var rowFilter = opts.csv_filter ? internal.compileExpressionToFunction(opts.csv_filter, {returns: true}) : null;
+  var colFilter = opts.csv_fields ? internal.getRecordMapper(internal.mapFieldNames(opts.csv_fields)) : null;
   var ctx = internal.getBaseContext();
-  if (!recordFilter && !fieldFilter) return null;
-  return function(rec) {
-    var val;
-    try {
-      val = recordFilter ? recordFilter.call(null, rec, ctx) : true;
-    } catch(e) {
-      stop(e.name, "in expression [" + exp + "]:", e.message);
-    }
-    if (val === false) {
-      return null;
-    } else if (val !== true) {
-      stop("Filter expression must return true or false");
-    }
-    return fieldFilter ? fieldFilter(rec) : rec;
-  };
+  if (rowFilter) {
+    return function(rec) {
+      var val;
+      try {
+        val = rowFilter ? rowFilter.call(null, rec, ctx) : true;
+      } catch(e) {
+        stop(e.name, "in expression [" + exp + "]:", e.message);
+      }
+      if (val === false) {
+        return null;
+      } else if (val !== true) {
+        stop("Filter expression must return true or false");
+      }
+      // use in conjunction with column filter if present
+      return colFilter ? colFilter(rec) : rec;
+    };
+  } else if (colFilter) {
+    return colFilter;
+  } else {
+    return null;
+  }
 };
 
 
