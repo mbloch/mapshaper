a83ecbc737321f549cf8f6ff3bc28559ecdc2c12
Matthew Bloch <masiyou@gmail.com>
2019-09-30 23:36:36 -0400

    Update website

--- mapshaper.js
+++ mapshaper.js
@@ -1,5 +1,5 @@
 (function(){
-VERSION = '0.4.133';
+VERSION = '0.4.134';
 
 var error = function() {
   var msg = utils.toArray(arguments).join(' ');
@@ -5322,6 +5322,12 @@ internal.getDelimFieldFilter = function(header, fieldsToKeep) {
   var map = header.map(function(name) {
     return name in index;
   });
+  var missing = utils.difference(fieldsToKeep, header);
+  if (missing.length > 0) {
+    var foundStr = [''].concat(header).join('\n  ');
+    var missingStr = [''].concat(missing).join('\n  ');
+    stop('csv-fields option has', missing.length == 1 ? 'a name' : missing.length + ' names',  'not found in the file\nFields:', foundStr, '\nMissing:', missingStr);
+  }
   return function(colIdx) {
     return map[colIdx];
   };
