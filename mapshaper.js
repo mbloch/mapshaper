051479578e2100996d1a1c682c223232bd26fc0a
Matthew Bloch <masiyou@gmail.com>
2019-12-26 22:19:13 -0500

    Update website

--- mapshaper.js
+++ mapshaper.js
@@ -1,5 +1,5 @@
 (function(){
-VERSION = '0.4.151';
+VERSION = '0.4.152';
 
 var error = function() {
   var msg = utils.toArray(arguments).join(' ');
@@ -19289,8 +19289,8 @@ internal.identifyJSONString = function(str, opts) {
   var fmt = null;
   if (str.length > maxChars) str = str.substr(0, maxChars);
   str = str.replace(/\s/g, '');
-  if (opts && opts.json_subtree) {
-    fmt = 'json'; // TODO: make json_subtree compatible with other types
+  if (opts && opts.json_path) {
+    fmt = 'json'; // TODO: make json_path compatible with other types
   } else if (/^\[[{\]]/.test(str)) {
     // empty array or array of objects
     fmt = 'json';
@@ -19310,7 +19310,7 @@ internal.identifyJSONObject = function(o) {
     fmt = 'topojson';
   } else if (o.type) {
     fmt = 'geojson';
-  } else if (utils.isArray(o) || o.json_subtree) {
+  } else if (utils.isArray(o) || o.json_path) {
     fmt = 'json';
   }
   return fmt;
@@ -19380,10 +19380,10 @@ internal.importJSON = function(data, opts) {
         stop('JSON parsing error --', e.message);
       }
     }
-    if (opts.json_subtree) {
-      content = internal.selectFromObject(content, opts.json_subtree);
+    if (opts.json_path) {
+      content = internal.selectFromObject(content, opts.json_path);
       if (Array.isArray(content) === false) {
-        stop('Expected an array at JSON subtree:', opts.json_subtree);
+        stop('Expected an array at JSON path:', opts.json_path);
       }
     }
     retn.format = internal.identifyJSONObject(content, opts);
@@ -25732,8 +25732,8 @@ internal.getOptionParser = function() {
       type: 'strings',
       describe: '[CSV] comma-sep. list of fields to import'
     })
-    .option('json-subtree', {
-      old_alias: 'json-path',
+    .option('json-path', {
+      old_alias: 'json-subtree',
       describe: '[JSON] path to an array of data records; separator is /'
     });
 
