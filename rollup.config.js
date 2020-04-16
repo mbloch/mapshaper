0656c330016ac32cdee2be433390abc4f7987e8e
Matthew Bloch <masiyou@gmail.com>
2020-04-16 08:20:20 -0400

    Tweak build process

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,29 @@
+
+const onBundle = {
+  name: 'onbundle',
+  buildEnd() {
+    // copy mapshaper.js to www/
+    const fs = require('fs');
+    fs.writeFileSync('www/mapshaper.js', fs.readFileSync('mapshaper.js'));
+  }
+};
+
 export default [{
-  input: 'src/mapshaper.js',
-  output: [{
-    strict: false,
-    format: 'iife',
-    file: 'mapshaper.js',
-    intro: 'var VERSION = "' + require('./package.json').version + '";\n'
-  }]
-}, {
+  treeshake: false,
   input: 'src/gui/gui.js',
   output: [{
     strict: false,
     format: 'iife',
     file: 'www/mapshaper-gui.js'
   }]
+}, {
+  treeshake: false,
+  input: 'src/mapshaper.js',
+  output: [{
+    strict: false,
+    format: 'iife',
+    file: 'mapshaper.js',
+    intro: 'var VERSION = "' + require('./package.json').version + '";\n'
+  }],
+  plugins: [onBundle]
 }];
