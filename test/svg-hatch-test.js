69335e66e77cd6544fb14b222f120f2a5598d960
Matthew Bloch <masiyou@gmail.com>
2020-08-15 19:31:22 -0400

    Add tests

---
+++ test/svg-hatch-test.js
@@ -0,0 +1,36 @@
+import { parseHatch } from '../src/svg/svg-hatch.js'
+import assert from 'assert';
+
+describe('svg-hatch.js', function () {
+  describe('parseHatch()()', function () {
+    it('0 #eee 2 black 1', function () {
+      assert.deepEqual(parseHatch('0 #eee 2 black 1'), {
+        colors: ['#eee', 'black'],
+        widths: [2, 1],
+        rotation: 0
+      })
+    })
+
+    it('45 deg is default rotation', function () {
+      assert.deepEqual(parseHatch('#444444 2 rgba(0,0,0) 2'), {
+        colors: ['#444444', 'rgba(0,0,0)'],
+        widths: [2, 2],
+        rotation: 45
+      });
+    })
+
+    it('supports more than 2 stripes', function () {
+      assert.deepEqual(parseHatch('90deg green 5 gold 2 black 9'), {
+        colors: ['green', 'gold', 'black'],
+        widths: [5, 2, 9],
+        rotation: 90
+      });
+    })
+
+    it('invalid stripe width', function () {
+      assert.strictEqual(parseHatch('#eee 0 black 1'), null);
+    })
+
+  })
+})
+
