0d3007055159c8371116c777e15ad392f833f139
Matthew Bloch <masiyou@gmail.com>
2021-04-13 13:43:03 -0400

    Update website

--- mapshaper.js
+++ mapshaper.js
@@ -1,6 +1,6 @@
 (function () {
 
-  var VERSION = "0.5.41";
+  var VERSION = "0.5.42";
 
 
   var utils = /*#__PURE__*/Object.freeze({
@@ -15465,13 +15465,9 @@ ${svg}
         describe: 'radius of each dot in pixels',
         type: 'number'
       })
-      .option('spacing', {
-        describe: 'how evenly dots should be spaced (0-1, default is 1)',
+      .option('evenness', {
+        describe: '(0-1) 0 is random placement, 1 is even placement; default is 1',
         type: 'number'
-      })
-      .option('random', {
-        describe: 'place dots randomly instead of spreading them out',
-        type: 'flag'
       })
       .option('target', targetOpt)
       .option('name', nameOpt)
@@ -26377,6 +26373,15 @@ ${svg}
   // TODO: optimize point-in-polygon tests for complex polygons and many points
   // import { PathIndex } from '../paths/mapshaper-path-index';
 
+  function placeDotsInPolygon(shp, arcs, n, opts) {
+    var evenness = opts.evenness >= 0 ? Math.min(opts.evenness, 1) : 1;
+    // TODO: also skip tiny sliver polygons?
+    if (n === 0) return [];
+    if (evenness === 0) return placeDotsRandomly(shp, arcs, n);
+    // TODO: if n == 1, consider using the 'inner' point
+    return placeDotsEvenly(shp, arcs, n, evenness);
+  }
+
   function placeDotsRandomly(shp, arcs, n) {
     var bounds = arcs.getMultiShapeBounds(shp);
     var coords = [];
@@ -26400,16 +26405,12 @@ ${svg}
     return null;
   }
 
-
-  function placeDotsEvenly(shp, arcs, n, opts) {
-    if (n === 0) return [];
+  function placeDotsEvenly(shp, arcs, n, evenness) {
     var shpArea = geom.getPlanarShapeArea(shp, arcs);
-    // TODO: also skip tiny sliver polygons?
     if (shpArea > 0 === false) return [];
     var bounds = arcs.getMultiShapeBounds(shp);
-    var approxCells = Math.round(n * bounds.area() / shpArea);
-    var evenness = opts.spacing >= 0 ? Math.min(opts.spacing, 1) : 1;
-    var grid = new DotGrid(bounds, approxCells, evenness);
+    var approxQueries = Math.round(n * bounds.area() / shpArea);
+    var grid = new DotGrid(bounds, approxQueries, evenness);
     var coords = [];
     for (var i=0; i<n; i++) {
       coords.push(placeDot(shp, arcs, grid));
@@ -26426,73 +26427,71 @@ ${svg}
       p = grid.getPoint();
       if (!p) continue;
       if (testPointInPolygon(p[0], p[1], shp, arcs)) {
-        grid.usePoint(p);
         return p;
-      } else {
-        grid.rejectPoint(p);
       }
     }
     return null;
   }
 
-  function DotGrid(bounds, approxCells, evenness) {
+  function DotGrid(bounds, approxQueries, evenness) {
     var grid = [];
     var x0 = bounds.xmin;
     var y0 = bounds.ymin;
     var w = bounds.width();
     var h = bounds.height();
+    var approxCells = approxQueries * 0.8;
     var cols = Math.round(Math.sqrt(approxCells * w / h));
     var rows = Math.ceil(cols * h / w); // overshoots bbox height
     var cells = cols * rows;
     var nextId = -1;
-    var queuedCells = [];
-    var probesBeforeRelaxation = Math.ceil(cells) / 2;
+    var bestPoints;
+    var probesBeforeRelaxation = Math.ceil(Math.pow(cells, 0.8));
     var maxProbes = cells * 10;
-    var probedPoints = 0;
     var queries = 0;
-    var api = {};
 
     // set height to height of grid
     h = w * rows / cols;
 
+    var k = Math.pow(evenness, 0.7); // seems to make a better transition
     // Estimate of minimum distance between dots (based on a square grid)
-    // 0.6 seems adequate from trial-and-error... consider adding a bit more to
-    // be on the safe side. (run time increases fast)
-    var minDist = w / cols * 0.6 * evenness;
+    // 0.7 seems about right from trial-and-error...
+    var minDist = w / cols * 0.7 * k;
 
-    api.done = function() {
-      // console.log(`queries: ${queries}, probed: ${Math.round(probedPoints / queries * 100)}%`);
-    };
+    return {done: done, getPoint: getPoint};
 
-    api.getPoint = function() {
-      var probes = 0;
-      var p, id;
+    function done() {
+      // TODO: check placement metrics if debugging
+    }
+
+    function getPoint() {
       queries++;
-      // first, try finding a point in each grid cell
+      if (evenness === 1) return getOptimalPoint();
+      if (evenness === 0) return getRandomPoint();
+      return getSpacedPoint();
+    }
+
+    function getOptimalPoint() {
+      var p;
+      // first, try to place a point in each grid cell
       // (for fast and even initial dot placement)
       while (++nextId < cells) {
         p = getRandomPointInCell(nextId);
-        if (pointIsUsable(p)) return p;
-        queuedCells.push(nextId);
-        // console.log('failed', nextId)
+        if (pointIsUsable(p)) {
+          return usePoint(p);
+        }
       }
-      // this gives one more chance to fill empty cells that had
-      // collisions during the first pass
-      // (is this worth doing? does it tend to create clumping?)
-      while (queuedCells.length > 0) {
-        id = queuedCells.pop();
-        p = getRandomPointInCell(id);
-        // console.log('retry?', pointIsUsable(p))
-        if (pointIsUsable(p)) return p;
-        // if (Math.random() > 0.3) queuedCells.push(id);
-      }
+      // assumes that the point grid has already been seeded
+      return useBestPoint();
+    }
 
-      // random dart-throwing
+    function getSpacedPoint() {
+      // use dart-throwing, reject points that are within the minimum distance
+      var probes = 0;
+      var p;
       while (probes++ < maxProbes) {
         p = getRandomPoint();
         if (pointIsUsable(p)) {
-          probedPoints++;
-          return p;
+          return usePoint(p);
         }
         if (probes % probesBeforeRelaxation === 0) {
           // relax min dist after a number of failed probes
@@ -26500,28 +26499,127 @@ ${svg}
         }
       }
       return null;
-    };
-
-    api.rejectPoint = function(xy) {
-      addPointToCell(xy);
-    };
-
-    api.usePoint = function(xy) {
-      addPointToCell(xy);
-    };
-
-    return api;
-
-    function getRandomPointInCell(i) {
-      var r = getRow(i);
-      var c = getCol(i);
-      var x = (Math.random() + r) / rows * w + x0;
-      var y = (Math.random() + c) / cols * h + y0;
-      return [x, y];
     }
 
-    function getJitteredCoord(i, n, range, offs) {
-      return (Math.random() + i) / n * range + offs;
+    // Add point to grid of used points
+    function usePoint(p) {
+      var i = pointToIdx(p);
+      var points = grid[i] || (grid[i] = []);
+      points.push(p);
+      return p;
+    }
+
+    function useBestPoint() {
+      var maxDist = 0;
+      var bestId, p;
+      if (!bestPoints) {
+        bestPoints = initBestPoints();
+      }
+      for (var i=0; i<cells; i++) {
+        p = bestPoints[i];
+        if (p[2] > maxDist) {
+          maxDist = p[2];
+          bestId = i;
+        }
+      }
+      p = bestPoints[bestId].slice(0, 2); // remove distance member
+      usePoint(p); // add to grid of used points
+      updateBestPoints(p, bestId); // update best point of this cell and neighbors
+      return p;
+    }
+
+    function initBestPoints() {
+      var bestPoints = [];
+      for (var i=0; i<cells; i++) {
+        bestPoints.push(findBestPointInCell(i));
+      }
+      return bestPoints;
+    }
+
+    function updateBestPoints(p, i) {
+      var r = idxToRow(i);
+      var c = idxToCol(i);
+      bestPoints[i] = findBestPointInCell(i);
+      updateBestPoint(p, c+1, r);
+      updateBestPoint(p, c, r+1);
+      updateBestPoint(p, c-1, r);
+      updateBestPoint(p, c, r-1);
+      updateBestPoint(p, c+1, r+1);
+      updateBestPoint(p, c-1, r+1);
+      updateBestPoint(p, c-1, r-1);
+      updateBestPoint(p, c+1, r-1);
+    }
+
+    function updateBestPoint(addedPt, c, r) {
+      var i = colRowToIdx(c, r);
+      if (i == -1) return;
+      var bestPt = bestPoints[i];
+      // don't need to update best point if the newly added point is too far away
+      // to have an effect
+      if (pointToPointDistSq(addedPt, bestPt) > bestPt[2]) return;
+      bestPoints[i] = findBestPointInCell(i);
+    }
+
+    function findBestPointInCell(i) {
+      var probes = 30;
+      var maxDist = 0;
+      var best, p, dist;
+      while (probes-- > 0) {
+        p = getRandomPointInCell(i);
+        dist = findDistanceFromNeighbors(p, i);
+        if (dist > maxDist) {
+          maxDist = dist;
+          best = p;
+        }
+      }
+      best.push(maxDist); // add distance as third element in the point
+      return best;
+    }
+
+    function findDistanceFromNeighbors(xy, i) {
+      var dist = Infinity;
+      var r = idxToRow(i);
+      var c = idxToCol(i);
+      dist = reduceDistance(dist, xy, c, r);
+      dist = reduceDistance(dist, xy, c+1, r);
+      dist = reduceDistance(dist, xy, c, r+1);
+      dist = reduceDistance(dist, xy, c-1, r);
+      dist = reduceDistance(dist, xy, c, r-1);
+      dist = reduceDistance(dist, xy, c+1, r+1);
+      dist = reduceDistance(dist, xy, c+1, r-1);
+      dist = reduceDistance(dist, xy, c-1, r+1);
+      dist = reduceDistance(dist, xy, c-1, r-1);
+      return dist;
+    }
+
+    function reduceDistance(memo, xy, c, r) {
+      var i = colRowToIdx(c, r);
+      if (i == -1) return memo; // off the edge
+      var distSq = pointToPointsDistSq(xy, grid[i] || []);
+      return Math.min(memo, distSq);
+    }
+
+    function pointToPointsDistSq(xy, points) {
+      var dist = Infinity;
+      for (var i=0; i<points.length; i++) {
+        dist = Math.min(dist, pointToPointDistSq(xy, points[i]));
+      }
+      return dist;
+    }
+
+    function pointToPointDistSq(a, b) {
+      var dx = a[0] - b[0];
+      var dy = a[1] - b[1];
+      return dx * dx + dy * dy;
+    }
+
+    function getRandomPointInCell(i) {
+      var r = idxToRow(i);
+      var c = idxToCol(i);
+      var x = (Math.random() + c) / cols * w + x0;
+      var y = (Math.random() + r) / rows * h + y0;
+      var p = [x, y];
+      return p;
     }
 
     function getRandomPoint() {
@@ -26532,25 +26630,9 @@ ${svg}
       return Math.floor(Math.random() * cells);
     }
 
-
-    function addPointToCell(xy) {
-      var i = getCellIdx(xy);
-      var points = grid[i];
-      if (!points) {
-        points= grid[i] = [];
-      }
-      points.push(xy);
-    }
-
-    function getCellIdx(xy) {
-      var c = getPointCol(xy);
-      var r = getPointRow(xy);
-      return r * cols + c;
-    }
-
     function pointIsUsable(xy) {
-      var c = getPointCol(xy),
-          r = getPointRow(xy);
+      var c = pointToCol(xy),
+          r = pointToRow(xy);
       var collision = testCollision(xy, c, r) ||
         testCollision(xy, c+1, r) ||
         testCollision(xy, c, r+1) ||
@@ -26564,47 +26646,56 @@ ${svg}
     }
 
     function testCollision(xy, c, r) {
-      if (c < 0 || r < 0 || c >= cols || r >= cols) return false;
-      var i = r * cols + c;
+      var i = colRowToIdx(c, r);
+      if (i == -1) return false;
       var points = grid[i];
       if (!points || !testPointCollision(xy, points, minDist)) return false;
       return true;
     }
 
     function testPointCollision(xy, points, dist) {
+      var d2 = dist * dist;
       for (var i=0; i<points.length; i++) {
-        if (pointsAreTooClose(xy, points[i], dist)) {
+        if (pointToPointDistSq(xy, points[i]) < d2) {
           return true;
         }
       }
       return false;
     }
 
-    function pointsAreTooClose(a, b, dist) {
-      var dx = a[0] - b[0];
-      var dy = a[1] - b[1];
-      return dx * dx + dy * dy < dist * dist;
+    function pointToCol(xy) {
+      var dx = xy[0] - x0;
+      var c = Math.floor(dx / w * cols);
+      if (c < 0) c = 0;
+      if (c >= cols) c = cols-1;
+      return c;
     }
 
-    function getPointCol(xy) {
-      var x = xy[0];
-      var dx = x - x0;
-      var c = Math.floor(dx / w);
-      return c; // todo: validate or clamp
+    function pointToRow(xy) {
+      var dy = xy[1] - y0;
+      var r = Math.floor(dy / h * rows);
+      if (r < 0) r = 0;
+      if (r >= rows) r = rows-1;
+      return r;
     }
 
-    function getPointRow(xy) {
-      var y = xy[1];
-      var dy = y - y0;
-      var r = Math.floor(dy / h);
-      return r; // todo: validate or clamp
+    function colRowToIdx(c, r) {
+      if (c < 0 || r < 0 || c >= cols || r >= rows) return -1;
+      return r * cols + c;
     }
 
-    function getCol(i) {
+    function pointToIdx(xy) {
+      var c = pointToCol(xy);
+      var r = pointToRow(xy);
+      var idx = r * cols + c;
+      return idx;
+    }
+
+    function idxToCol(i) {
       return Math.floor(i % cols);
     }
 
-    function getRow(i) {
+    function idxToRow(i) {
       return Math.floor(i / cols);
     }
   }
@@ -26661,12 +26752,13 @@ ${svg}
     // TODO: instead of random shuffling, interleave dot classes more regularly?
     shuffle(indexes);
     var idx, prevIdx = -1;
+    var grouped = false;
     var coords;
     for (var i=0; i<dots.length; i++) {
       var p = dots[i];
       if (!p) continue;
       idx = indexes[i];
-      if (idx != prevIdx) {
+      if (!grouped || idx != prevIdx) {
         prevIdx = idx;
         retn.shapes.push(coords = []);
         retn.attributes.push(getDataRecord(idx, opts));
@@ -26677,13 +26769,12 @@ ${svg}
   }
 
   function placeDots(shp, arcs, n, opts) {
-    var makeDots = opts.random ? placeDotsRandomly : placeDotsEvenly;
     // split apart multipart polygons for more efficient dot placement
     var polys = shp.length > 1 ? explodePolygon(shp, arcs) : [shp];
     var counts = apportionDotsByArea(polys, arcs, n);
     var dots = [];
     for (var i=0; i<polys.length; i++) {
-      dots = dots.concat(makeDots(polys[i], arcs, counts[i], opts));
+      dots = dots.concat(placeDotsInPolygon(polys[i], arcs, counts[i], opts));
     }
     return dots;
   }
