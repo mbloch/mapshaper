389d09fd7c37ac128143af272c0349c01f71f98f
Matthew Bloch <masiyou@gmail.com>
2021-04-14 20:05:58 -0400

    Add test

---
+++ test/dots-test.js
@@ -0,0 +1,22 @@
+import {
+  getDataRecord
+} from '../src/commands/mapshaper-dots';
+
+var api = require('../'),
+    assert = require('assert');
+
+describe('mapshaper-dots.js', function () {
+  describe('getDataRecord()', function () {
+    it('copy_fields option', function () {
+      var out = getDataRecord(0, {foo: 2, bar: 'a'}, {copy_fields: ['foo', 'bar']});
+      assert.deepEqual(out, {foo: 2, bar: 'a'});
+    })
+
+    it('colors option', function () {
+      var out = getDataRecord(1, null, {colors: ['red', 'green']});
+      assert.deepEqual(out, {fill: 'green', r: 1.5})
+    })
+
+  })
+
+})
