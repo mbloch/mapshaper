759a44d2313d765ed9ca3003ce438a84197e0f2c
Matthew Bloch <masiyou@gmail.com>
2021-05-11 21:24:07 -0400

    Fix default clipping circle for nsper

--- src/crs/mapshaper-proj-info.js
+++ src/crs/mapshaper-proj-info.js
@@ -26,18 +26,17 @@ export function getDefaultClipBBox(P) {
   }[getCrsSlug(P)] || null;
 }
 
-// TODO: add nsper, tpers
 export function isClippedAzimuthalProjection(P) {
   return inList(P, 'stere,sterea,ups,ortho,gnom,laea,nsper,tpers');
 }
 
 function getPerspectiveClipAngle(P) {
   var h = parseFloat(P.params.h.param);
-  if (!h || h < P.a) {
-
+  if (!h || h < 0) {
     return 0;
   }
-  var theta = Math.acos(P.a / h) * 180 / Math.PI;
+  var theta = Math.acos(P.a / (P.a + h)) * 180 / Math.PI;
+  theta *= 0.995; // reducing a bit to avoid out-of-range errors
   return theta;
 }
 
@@ -53,7 +52,7 @@ export function getDefaultClipAngle(P) {
     laea: 179,
     ortho: 90,
     stere: 142,
-    stereea: 142,
+    sterea: 142,
     ups: 10.5 // TODO: should be 6.5 deg at north pole
   }[slug] || 0;
 }
