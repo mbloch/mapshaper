9518967ea9b5bdc6556632b68f4cb7200077b4de
Matthew Bloch <masiyou@gmail.com>
2022-05-25 13:30:50 -0400

    Switch to eslint

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'node': true,
+        'es2021': true
+    },
+    'globals': {
+      'VERSION': 'readonly'
+      // 'module': 'readonly',
+      // 'require': 'readonly'
+    },
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'sourceType': 'module'
+    },
+    'rules': {
+        'no-prototype-builtins': ['off'], // allow obj.hasOwnProperty()
+        'no-control-regex': ['off'],
+        'no-empty': ['off'], // allow try { ... } catch(e) {}
+        'no-constant-condition': ['off'], // allow while(true)
+        'no-unused-vars': [
+          'warn'
+        ],
+        // 'semi': [
+        //   'error',
+        //   'always'
+        // ],
+        'indent': [
+            'off', // indent rule is troublesome
+            2
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'off',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ]
+    }
+};
