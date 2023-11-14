4807a8b6810bc78c0fc650cdd8b37c37563dc53d
Matthew Bloch <mbloch@nytimes.com>
2023-11-14 14:01:36 -0500

    Added target.geojson getter to run command

--- src/expressions/mapshaper-target-proxy.mjs
+++ src/expressions/mapshaper-target-proxy.mjs
@@ -1,9 +1,25 @@
 import { getLayerInfo } from '../commands/mapshaper-info';
+import { exportLayerAsGeoJSON } from '../geojson/geojson-export';
+import { addGetters } from '../expressions/mapshaper-expression-utils';
+// import { importGeoJSON } from '../geojson/geojson-import';
 
 export function getTargetProxy(target) {
   var lyr = target.layers[0];
-  var data = getLayerInfo(lyr, target.dataset);
+  var data = getLayerInfo(lyr, target.dataset); // layer_name, feature_count etc
   data.layer = lyr;
   data.dataset = target.dataset;
+  addGetters(data, {
+    // export as an object, not a string or buffer
+    geojson: getGeoJSON
+  });
+
+  function getGeoJSON() {
+    var features = exportLayerAsGeoJSON(lyr, target.dataset, {}, true);
+    return {
+      type: 'FeatureCollection',
+      features: features
+    };
+  }
+
   return data;
 }
