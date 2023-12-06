865291a1dfa6375b69522b2ed8699b70263d56d4
Matthew Bloch <mbloch@nytimes.com>
2023-12-05 21:05:45 -0500

    Switch to on-demand intersection testing in gui

--- src/gui/gui-map-utils.mjs
+++ src/gui/gui-map-utils.mjs
@@ -19,6 +19,32 @@ export function mapNeedsReset(newBounds, prevBounds, viewportBounds, flags) {
   return false;
 }
 
+// Test if an update may have affected the visible shape of arcs
+// @flags Flags from update event
+export function arcsMayHaveChanged(flags) {
+  return flags.simplify_method || flags.simplify || flags.proj ||
+    flags.arc_count || flags.repair || flags.clip || flags.erase ||
+    flags.slice || flags.affine || flags.rectangle || flags.buffer ||
+    flags.union || flags.mosaic || flags.snap || flags.clean || flags.drop || false;
+}
+
+// check for operations that may change the number of self intersections in the
+// target layer.
+export function intersectionsMayHaveChanged(flags) {
+  return arcsMayHaveChanged(flags) || flags.select || flags['merge-layers'] ||
+  flags.filter || flags.dissolve || flags.dissolve2;
+}
+
+// Test if an update allows hover popup to stay open
+export function popupCanStayOpen(flags) {
+  // keeping popup open after -drop geometry causes problems...
+  // // if (arcsMayHaveChanged(flags)) return false;
+  if (arcsMayHaveChanged(flags)) return false;
+  if (flags.points || flags.proj) return false;
+  if (!flags.same_table) return false;
+  return true;
+}
+
 // Returns proportion of bb2 occupied by bb1
 function getIntersectionPct(bb1, bb2) {
   return getBoundsIntersection(bb1, bb2).area() / bb2.area() || 0;
