5b0e1857071ded65e341c276bb5796419d05f5b5
Matthew Bloch <mbloch@nytimes.com>
2024-10-07 15:03:13 -0400

    Allow sub-pixel fill effect styles (e.g. hatches)

--- src/gui/gui-context-menu.mjs
+++ src/gui/gui-context-menu.mjs
@@ -2,17 +2,44 @@ import { internal, mapshaper, geom } from './gui-core';
 import { El } from './gui-el';
 import { saveFileContentToClipboard } from './gui-export-control';
 import { deleteFeature } from './gui-drawing-utils';
+import { GUI } from './gui-lib';
 
-export function ContextMenu() {
+
+var openMenu;
+
+document.addEventListener('mousedown', function(e) {
+  if (e.target.classList.contains('contextmenu-item')) {
+    return; // don't close menu if clicking on a menu link
+  }
+  closeOpenMenu();
+});
+
+function closeOpenMenu() {
+  if (openMenu) {
+    openMenu.close();
+    openMenu = null;
+  }
+}
+
+export function openContextMenu(e, lyr, parent) {
+  var menu = new ContextMenu(parent);
+  closeOpenMenu();
+  menu.open(e, lyr);
+}
+
+export function ContextMenu(parentArg) {
   var body = document.querySelector('body');
-  var menu = El('div').addClass('contextmenu rollover').appendTo(body);
+  var parent = parentArg || body;
+  // var menu = El('div').addClass('contextmenu rollover').appendTo(body);
+  var menu = El('div').addClass('contextmenu rollover').appendTo(parent);
   var _open = false;
   var _openCount = 0;
-  document.addEventListener('mousedown', close);
 
   this.isOpen = function() {
     return _open;
   };
+
+  this.close = close;
 
   function close() {
     var count = _openCount;
@@ -25,15 +52,18 @@ export function ContextMenu() {
     }, 200);
   }
 
-  function addMenuItem(label, func) {
-    var prefix = '• &nbsp;';
-
-    El('div')
+  function addMenuItem(label, func, prefixArg) {
+    var prefix = prefixArg === undefined ? '• &nbsp;' : prefixArg;
+    var item = El('div')
       .appendTo(menu)
       .addClass('contextmenu-item')
       .html(prefix + label)
-      .on('click', func)
       .show();
+
+    GUI.onClick(item, function(e) {
+      func();
+      closeOpenMenu();
+    });
   }
 
   function addMenuLabel(label) {
@@ -45,54 +75,88 @@ export function ContextMenu() {
 
   this.open = function(e, lyr) {
     var copyable = e.ids?.length;
-    if (lyr && !lyr.gui.geographic) return; // no popup for tabular data
+    if (_open) close();
     menu.empty();
 
-    // menu contents
-    //
-    if (e.deleteVertex || e.deletePoint || copyable || e.deleteFeature) {
+    if (openMenu && openMenu != this) {
+      openMenu.close();
+    }
+    openMenu = this;
 
-      addMenuLabel('selection');
-      if (e.deleteVertex) {
-        addMenuItem('delete vertex', e.deleteVertex);
-      }
-      if (e.deletePoint) {
-        addMenuItem('delete point', e.deletePoint);
-      }
-      if (e.ids?.length) {
-        addMenuItem('copy as GeoJSON', copyGeoJSON);
-      }
-      if (e.deleteFeature) {
-        addMenuItem(getDeleteLabel(), e.deleteFeature);
-      }
+    if (e.deleteLayer) {
+     addMenuItem('delete layer', e.deleteLayer, '');
+    }
+    if (e.selectLayer) {
+     addMenuItem('select layer', e.selectLayer, '');
     }
 
-    if (e.lonlat_coordinates) {
-      addMenuLabel('longitude, latitude');
-      addCoords(e.lonlat_coordinates);
-    }
-    if (e.projected_coordinates) {
-      addMenuLabel('x, y');
-      addCoords(e.projected_coordinates);
+    if (lyr && lyr.gui.geographic) {
+      if (e.deleteVertex || e.deletePoint || copyable || e.deleteFeature) {
+
+        addMenuLabel('selection');
+        if (e.deleteVertex) {
+          addMenuItem('delete vertex', e.deleteVertex);
+        }
+        if (e.deletePoint) {
+          addMenuItem('delete point', e.deletePoint);
+        }
+        if (e.ids?.length) {
+          addMenuItem('copy as GeoJSON', copyGeoJSON);
+        }
+        if (e.deleteFeature) {
+          addMenuItem(getDeleteLabel(), e.deleteFeature);
+        }
+      }
+
+      if (e.lonlat_coordinates) {
+        addMenuLabel('longitude, latitude');
+        addCoords(e.lonlat_coordinates);
+      }
+      if (e.projected_coordinates) {
+        addMenuLabel('x, y');
+        addCoords(e.projected_coordinates);
+      }
     }
 
     if (menu.node().childNodes.length === 0) {
       return;
     }
 
-    _open = true;
-    _openCount++;
     var rspace = body.clientWidth - e.pageX;
+    var offs = getParentOffset();
     var xoffs = 10;
     if (rspace > 150) {
-      menu.css('left', e.pageX + xoffs + 'px');
+      menu.css('left', e.pageX - offs.left + xoffs + 'px');
       menu.css('right', null);
     } else {
-      menu.css('right', (body.clientWidth - e.pageX + xoffs) + 'px');
+      menu.css('right', (body.clientWidth - e.pageX - offs.left + xoffs) + 'px');
       menu.css('left', null);
     }
-    menu.css('top', (e.pageY - 15) + 'px');
+    menu.css('top', (e.pageY - offs.top - 15) + 'px');
     menu.show();
+
+    _open = true;
+    _openCount++;
+
+    function getParentOffset() { // crossbrowser version
+      if (parent == body) {
+        return {top: 0, left: 0};
+      }
+
+      var box = parent.getBoundingClientRect();
+      var docEl = document.documentElement;
+
+      var scrollTop = window.pageYOffset || docEl.scrollTop || body.scrollTop;
+      var scrollLeft = window.pageXOffset || docEl.scrollLeft || body.scrollLeft;
+
+      var clientTop = docEl.clientTop || body.clientTop || 0;
+      var clientLeft = docEl.clientLeft || body.clientLeft || 0;
+
+      var top  = box.top +  scrollTop - clientTop;
+      var left = box.left + scrollLeft - clientLeft;
+
+      return { top: Math.round(top), left: Math.round(left) };
+    }
 
     function getDeleteLabel() {
       return 'delete ' + (lyr.geometry_type == 'point' ? 'point' : 'shape');
