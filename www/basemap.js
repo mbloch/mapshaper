a1f710302c51f4bc37e2b0542f3a418bb7448ab9
Matthew Bloch <mbloch@nytimes.com>
2025-06-28 11:14:45 -0400

    Update basemap api keys

--- www/basemap.js
+++ www/basemap.js
@@ -1,7 +1,8 @@
 window.mapboxParams = {
   js: 'https://api.mapbox.com/mapbox-gl-js/v2.11.1/mapbox-gl.js',
   css: 'https://api.mapbox.com/mapbox-gl-js/v2.11.1/mapbox-gl.css',
-  key: 'pk.eyJ1IjoiZ3JhbW1hdGEiLCJhIjoiY2wxMzI4b3dqMDNhMjNpcDhzcmU0aGh5diJ9.m1lacx48pTYls-X3FVhG9g',
+  production_key: 'pk.eyJ1IjoiZ3JhbW1hdGEiLCJhIjoiY21jZ2RlbGk1MGhvNzJrcHV2dTYzZWE2byJ9.btkRnRrPW75c0AlQ5lH2cg',
+  localhost_key: 'pk.eyJ1IjoiZ3JhbW1hdGEiLCJhIjoiY21jZ2Rna2hrMGo2ZjJqcHFxMHAycDlyMSJ9.q8FchhB-NAN_FGBHN0AJeg',
   styles: [{
     name: 'Map',
     icon: 'images/thumb-map.jpg',
