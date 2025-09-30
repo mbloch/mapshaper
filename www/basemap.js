8c4605331b30a715d5db60bbbdaf522b8a110f76
Matthew Bloch <mbloch@nytimes.com>
2025-09-29 21:41:41 -0400

    Add custom basemaps to web UI

--- www/basemap.js
+++ www/basemap.js
@@ -1,14 +1,16 @@
 window.mapboxParams = {
-  js: 'https://api.mapbox.com/mapbox-gl-js/v2.11.1/mapbox-gl.js',
-  css: 'https://api.mapbox.com/mapbox-gl-js/v2.11.1/mapbox-gl.css',
+  // js: 'https://api.mapbox.com/mapbox-gl-js/v2.11.1/mapbox-gl.js',
+  // css: 'https://api.mapbox.com/mapbox-gl-js/v2.11.1/mapbox-gl.css',
+  js: 'https://api.mapbox.com/mapbox-gl-js/v3.15.0/mapbox-gl.js',
+  css: 'https://api.mapbox.com/mapbox-gl-js/v3.15.0/mapbox-gl.css',
   production_key: 'pk.eyJ1IjoiZ3JhbW1hdGEiLCJhIjoiY21jZ2RlbGk1MGhvNzJrcHV2dTYzZWE2byJ9.btkRnRrPW75c0AlQ5lH2cg',
   localhost_key: 'pk.eyJ1IjoiZ3JhbW1hdGEiLCJhIjoiY21jZ2Rna2hrMGo2ZjJqcHFxMHAycDlyMSJ9.q8FchhB-NAN_FGBHN0AJeg',
   styles: [{
-    name: 'Map',
+    name: 'Reference map',
     icon: 'images/thumb-map.jpg',
     url: 'mapbox://styles/grammata/cl0ymri0f000214lc1mgtweqk'
   }, {
-    name: 'Satellite',
+    name: 'Satellite image',
     icon: 'images/thumb-satellite.jpg',
     dark: true,
     url: 'mapbox://styles/grammata/cl0ymvkkl004115mlgf5o442n'
