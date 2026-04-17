4cc0ce40ebc7ab3ecddfd39e3a1ca149b6b845a1
Matthew Bloch <mbloch@nytimes.com>
2026-04-17 14:52:11 -0400

    Update website

--- mapshaper.js
+++ mapshaper.js
@@ -51131,7 +51131,7 @@ ${svg}
     });
   }
 
-  var version = "0.6.118";
+  var version = "0.6.119";
 
   // Parse command line args into commands and run them
   // Function takes an optional Node-style callback. A Promise is returned if no callback is given.
