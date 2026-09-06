9286f95d9406aa2e889efc646a7a8f4b5f94e15c
Matthew Bloch <mbloch@nytimes.com>
2026-09-05 20:56:37 -0400

    Update website

--- basemap.js
+++ basemap.js
@@ -3,7 +3,7 @@ window.mapboxParams = {
   // css: 'https://api.mapbox.com/mapbox-gl-js/v2.11.1/mapbox-gl.css',
   js: 'https://api.mapbox.com/mapbox-gl-js/v3.15.0/mapbox-gl.js',
   css: 'https://api.mapbox.com/mapbox-gl-js/v3.15.0/mapbox-gl.css',
-  production_key: 'pk.eyJ1IjoiZ3JhbW1hdGEiLCJhIjoiY21jZ2RlbGk1MGhvNzJrcHV2dTYzZWE2byJ9.btkRnRrPW75c0AlQ5lH2cg',
+  production_key: 'pk.eyJ1IjoiZ3JhbW1hdGEiLCJhIjoiY210cDNrcGhkMTBueDJ5cHV0bDM5Y3ZrcCJ9.-X1-hP_OQJrInyGp2zMcBQ',
   localhost_key: 'pk.eyJ1IjoiZ3JhbW1hdGEiLCJhIjoiY21jZ2Rna2hrMGo2ZjJqcHFxMHAycDlyMSJ9.q8FchhB-NAN_FGBHN0AJeg',
   styles: [{
     name: 'Reference map',
